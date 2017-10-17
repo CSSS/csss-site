b109e138f22643160c8ebdb28d0edc9333a18db0
Jace Manshadi <j_manshad@sfu.ca>
2017-10-17 04:19:58 +0000

    removed testapp and added the personal url and made it the index page

--- csss/csss/urls.py
+++ csss/csss/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^webapp/', include('webapp.urls')),
+    url(r'^$', include('personal.urls')),
 ]
