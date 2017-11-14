ac75607f70e49f46aef314c666502e29db90c27c
Jace Manshadi <j_manshad@sfu.ca>
2017-11-14 10:14:41 +0000

    added contact page and learn how to pass variables with jinja logic and customize url parser

--- csss/personal/views.py
+++ csss/personal/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 def index(request):
 	return render(request, 'personal/home.html')
+
+def contact(request):
+	return render(request, 'personal/basic.html', {'content':['If you would like to contact me, please email me', 'csss-webmaster@sfu.ca']})
 # Create your views here.
