eb61221b6d929dd6c6bb7eadcd53332aa5423b7d
Jace Manshadi <j_manshad@sfu.ca>
2017-11-14 10:17:51 +0000

    completed video 5 tutorial

--- csss/personal/views.py
+++ csss/personal/views.py
@@ -5,4 +5,3 @@ def index(request):
 
 def contact(request):
 	return render(request, 'personal/basic.html', {'content':['If you would like to contact me, please email me', 'csss-webmaster@sfu.ca']})
-# Create your views here.
