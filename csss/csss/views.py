76995c6afc9c93fd1512b9f1c7dbe3c18fe57842
modernNeo <j_manshad@sfu.ca>
2018-03-14 00:07:53 -0700

    trying to pull stored emails

--- csss/csss/views.py
+++ csss/csss/views.py
@@ -1,25 +1,18 @@
 from django.shortcuts import render
 from announcements.models import Post
 import announcements
-from django_mailbox.models import Mailbox
-from django.core.management.base import BaseCommand
+from django_mailbox.models import MessageAttachment, Message, Mailbox
 
 def index(request):
   print("announcements-index")
   object_list = Post.objects.all()
   print("1")
-  mailboxes = Mailbox.active_mailboxes.all()
-  print("mailboxes=[",end='')
-  print(mailboxes,end='')
+  messages = Message.objects.all()
+  print("messages=[",end='')
+  print(messages,end='')
   print("]")
-  for mailbox in mailboxes:
-    print('Gathering messages for %s',mailbox.name)
-    messages = mailbox.get_new_mail()
-    print("messages=[",end='')
-    print(messages,end='')
-    print("]")
-    for message in messages:
-      print('Received Subject=[%s] From=[%s] From=[%s] Body=[%s]',message.subject,message.from_header,message.from_address,message.body)
+  for message in messages:
+    print('Received Subject=['+message.subject+'] From=['+message.from_header+'] From=['+message.from_address+'] Body=['+message.body+']')
   return render(request, 'announcements/announcements.html', {'object_list': object_list})
 #	return render(request, 'announcements/announcements.html', {'content':['Hi you doiiiiiin?']})
 
