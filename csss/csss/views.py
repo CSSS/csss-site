e70bf5d37876dec3aed065d30030c421bac49586
modernNeo <j_manshad@sfu.ca>
2018-03-14 00:26:17 -0700

    changing what the script determines the end of the body

--- csss/csss/views.py
+++ csss/csss/views.py
@@ -18,7 +18,7 @@ def index(request):
     print(decoded_body,end='')
     print("]")
     indexOfFirst=decoded_body.index("UTF-8")
-    indexOfLast = decoded_body.index("<div><br clear=3D\"all\">")
+    indexOfLast = decoded_body.index("Content-Type: text/html")
     print("indexOfFirst=["+str(indexOfFirst)+"] indexOfLast+["+str(indexOfLast)+"] -- indexOfFirst-indexOfLast=["+str(indexOfFirst-indexOfLast)+"] -- length=["+str(len(decoded_body))+"]")
     print(decoded_body[indexOfFirst+8:indexOfLast].replace('\\n', '\n'))
     message.body=decoded_body[indexOfFirst+8:indexOfLast].replace('\\n', '\n')
