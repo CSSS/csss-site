74882b5923746cdd276dd141c2c915debe5a5b64
modernNeo <j_manshad@sfu.ca>
2018-03-13 22:43:58 -0700

    changing location where the emails are pulled from

--- csss/announcements/urls.py
+++ csss/announcements/urls.py
@@ -1,23 +1,6 @@
 from django.conf.urls import url, include
 from django.views.generic import ListView, DetailView
 from announcements.models import Post
-from django_mailbox.models import Mailbox
-
-mailboxes = Mailbox.active_mailboxes.all()
-for mailbox in mailboxes:
-  logger.info(
-    'Gathering messages for %s',
-    mailbox.name
-    )
-  messages = mailbox.get_new_mail()
-  for message in messages:
-    logger.info(
-      'Received Subject=[%s] From=[%s] From=[%s] Body=[%s]',
-      message.subject,
-      message.from_header,
-      message.from_address,
-      message.body
-      )
 
 urlpatterns= [
 	url(r'^(?P<pk>\d+)$',DetailView.as_view(model=Mailbox,template_name='announcements/post.html' )),
--- csss/csss/views.py
+++ csss/csss/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
 from announcements.models import Post
 import announcements
+from django_mailbox.models import Mailbox
 
 def index(request):
 	print("announcements index")
 	object_list = Post.objects.all()
+
+  mailboxes = Mailbox.active_mailboxes.all()
+  for mailbox in mailboxes:
+    logger.info(
+      'Gathering messages for %s',
+      mailbox.name
+      )
+    messages = mailbox.get_new_mail()
+  for message in messages:
+    logger.info(
+      'Received Subject=[%s] From=[%s] From=[%s] Body=[%s]',
+      message.subject,
+      message.from_header,
+      message.from_address,
+      message.body
+      )
+
 	return render(request, 'announcements/announcements.html', {'object_list': object_list})
 #	return render(request, 'announcements/announcements.html', {'content':['Hi you doiiiiiin?']})
 
