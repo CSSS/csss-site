62f346226c6cfcaf1b4f908d419c3dfdb39eec27
modernNeo <j_manshad@sfu.ca>
2018-03-13 22:40:02 -0700

    updating which columns get pulled from database for emails

--- csss/announcements/urls.py
+++ csss/announcements/urls.py
@@ -1,8 +1,25 @@
 from django.conf.urls import url, include
 from django.views.generic import ListView, DetailView
 from announcements.models import Post
+from django_mailbox.models import Mailbox
+
+mailboxes = Mailbox.active_mailboxes.all()
+for mailbox in mailboxes:
+  logger.info(
+    'Gathering messages for %s',
+    mailbox.name
+    )
+  messages = mailbox.get_new_mail()
+  for message in messages:
+    logger.info(
+      'Received Subject=[%s] From=[%s] From=[%s] Body=[%s]',
+      message.subject,
+      message.from_header,
+      message.from_address,
+      message.body
+      )
 
 urlpatterns= [
-	url(r'^(?P<pk>\d+)$',DetailView.as_view(model=Post,template_name='announcements/post.html' )),
-	url(r'^$',ListView.as_view(queryset=Post.objects.all().order_by("date")[:25],template_name="announcements/announcements.html"))
+	url(r'^(?P<pk>\d+)$',DetailView.as_view(model=Mailbox,template_name='announcements/post.html' )),
+	url(r'^$',ListView.as_view(queryset=Mailbox.objects.all().order_by("date")[:25],template_name="announcements/announcements.html"))
 ]
