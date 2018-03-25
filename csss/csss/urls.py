84e9e09da6516631b1a098ebb4d5d2238fb15cb8
modernNeo <j_manshad@sfu.ca>
2018-03-24 22:32:04 -0700

    updating urls.py to allow admin and admin/ to work

--- csss/csss/urls.py
+++ csss/csss/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-	url(r'^admin/', admin.site.urls),
+	url(r'^admin', admin.site.urls),
 	url(r'^blog/', include('blog.urls')),
 	url(r'^about/',include('about.urls')),
 	url(r'^documents/', include('documents.urls')),
