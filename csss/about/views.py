af4b3f0de0274f0649f4cd610c9f6d3428386625
modernNeo <j_manshad@sfu.ca>
2018-03-28 00:54:33 -0700

    removing unneded function redirects

--- csss/about/views.py
+++ csss/about/views.py
@@ -8,19 +8,3 @@ def listOfOfficers(request):
 	print("list of officers index")
 	return render(request, 'about/list_of_officers.html')
 # Create your views here.
-
-def fall_2017(request):
-  print("fall_2017 index")
-  return render(request, 'about/fall_2017.html')
-
-def spring_2018(request):
-  print("spring_2018 index")
-  return render(request, 'about/spring_2018.html')
-
-def summer_2017(request):
-  print("summer_2017 index")
-  return render(request, 'about/summer_2017.html')
-
-def spring_2017(request):
-  print("spring_2017 index")
-  return render(request, 'about/spring_2017.html')
