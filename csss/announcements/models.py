23bec4ddd4bf957bce525b9ee53d18ed603e53ad
modernNeo <j_manshad@sfu.ca>
2018-05-10 02:42:52 -0700

    fixing import

--- csss/announcements/models.py
+++ csss/announcements/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-import csss.announcements.utils
+from announcements import utils
 
 class Post(models.Model):
 	date=models.DateTimeField()
