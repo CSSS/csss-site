90e2c03e41dc6e03a874d9149df019e82aabcaa4
modernNeo <j_manshad@sfu.ca>
2018-05-10 16:21:47 -0700

    adding missing import

--- csss/announcements/models.py
+++ csss/announcements/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from announcements import utils
+import datetime
 
 class Post(models.Model):
 	subject = models.CharField(
