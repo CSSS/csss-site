631e7e0315c6e3bd69513f03c5375c8b4b8a1fff
modernNeo <j_manshad@sfu.ca>
2018-05-11 13:36:58 -0700

    playing around with static files

--- csss/csss/settings.py
+++ csss/csss/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    #'django.contrib.staticfiles',
     'bootstrap3',
 ]
 
