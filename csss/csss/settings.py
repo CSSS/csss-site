44359a4c99da52f4ce719f036e262b9565f95e32
modernNeo <j_manshad@sfu.ca>
2018-05-11 11:08:36 -0700

    playing around with static files

--- csss/csss/settings.py
+++ csss/csss/settings.py
@@ -67,7 +67,7 @@ print("STATIC_URL=["+str(STATIC_URL)+"]")
 
 STATICFILES_DIRS = [
     'csss/statics/csss',
-    'documents',
+    'documents/contact',
 ]
 
 print("STATICFILES_DIRS=["+str(STATICFILES_DIRS)+"]")
