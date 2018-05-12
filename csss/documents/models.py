b23801408026e87316f6748a9a11d769ac602be9
modernNeo <j_manshad@sfu.ca>
2018-05-12 00:25:28 -0700

    adding contact_form

--- csss/documents/models.py
+++ csss/documents/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from django import forms
+
 class documentsPage(models.Model):
 	title=models.CharField(max_length=140)
 	body=models.TextField()
@@ -7,3 +9,10 @@ class documentsPage(models.Model):
 	def __str__(self):
 		return self.title
 # Create your models here.
+
+class ContactForm(forms.Form):
+  contact_name = forms.CharField(required=True)
+  content = forms.CharField(
+    required = True,
+    widget=forms.Textarea
+  )
