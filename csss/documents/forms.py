48865b8f869dd445f166d7489b2132f00453d4da
modernNeo <j_manshad@sfu.ca>
2018-05-30 00:19:11 -0700

    adding missing import

--- csss/documents/forms.py
+++ csss/documents/forms.py
@@ -1,6 +1,6 @@
-from django.forms import ModelForm
+from django import forms
 
-class ContactForm(ModelForm):
+class ContactForm(forms.ModelForm):
   contact_name = forms.CharField(required=True)
   content = forms.CharField(
     required = True,
@@ -8,7 +8,7 @@ class ContactForm(ModelForm):
   )
   pics_from_event = forms.FileField(required=False,
     widget=forms.ClearableFileInput(attrs={'multiple': True}))
-  
+
   def save(self, commit=True):
   	name = self.cleaned_data['contact_name']
   	content = self.cleaned_data['content']
