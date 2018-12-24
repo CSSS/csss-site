751b860dd347082d44d4278fed33857fe5c59dc3
Jace Manshadi <j_manshad@sfu.ca>
2018-12-23 19:03:29 -0800

    removing the stuff that has been moved to its own app

--- csss/documents/views.py
+++ csss/documents/views.py
@@ -12,26 +12,8 @@ def policies(request):
 
 
 def photo_gallery(request):
-	if request.method == 'POST':
-		form = ContactForm(request.POST)
-		title = form['title']
-		#additional_info = form.cleaned_data['additional_info']
-		additional_info = form['additional_info']
-		#contact_info = form.cleaned_data['contact_info']
-		contact_info = form['contact_info']
-		#photos = request.FILES.getlist('pics_from_event')
-		photos = request.FILES.getlist('pics_from_event')
-		if photos is not None:
-			print("photos type=["+str(type(photos))+"]")
-			for photo in photos:
-				print("photo=["+str(photo)+"]")
-		else:
-			print("no pictures detected")
-	else:
-		form = ContactForm()
-		args = {'form': form}
-		print("photo_gallery not POST")
-		return render(request, 'documents/photo_gallery.html', args)
+	print("photo_gallery not POST")
+	return render(request, 'documents/photo_gallery.html')
 
 def photos(request):
 	print("photo gallery index")
