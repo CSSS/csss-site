2c3b6b9b0b1d5b97037efa4573d4b764ad93b004
Jace Manshadi <jaymanshadi@gmail.com>
2019-09-05 22:29:07 -0700

    removing unneeded apps

--- csss/personal/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-def index(request):
-	return render(request, 'personal/home.html')
-
-def contact(request):
-	return render(request, 'personal/basic.html', {'content':['If you would like to contact me, please email me', 'csss-webmaster@sfu.ca']})
