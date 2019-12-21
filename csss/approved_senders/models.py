b5bc315e835aab82ce37dee0ac684b643d7f488c
Jace Manshadi <jaymanshadi@gmail.com>
2019-12-21 01:31:04 -0800

    removing migrations

--- csss/approved_senders/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Sender(models.Model):
-	email=models.CharField(max_length=140)
-	name=models.CharField(max_length=140)
-
-	def __str__(self):
-		return self.email
