a620be8c852f3e373ecb14d78be370db86f17f61
Jace Manshadi <jaymanshadi@gmail.com>
2020-01-13 20:13:38 -0800

    fixing flake8 issue

--- csss-site/src/about/models.py
+++ csss-site/src/about/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 import datetime
 
+
 class SourceFile(models.Model):
     json_file = models.FileField(
         default='exec_positions/default',
