caee6a443b0a426178d996f101b5448390669c56
VJ <vijenderbakhshi@gmail.com>
2020-01-30 23:23:10 -0800

    site update for mm2020

--- csss-site/src/events/urls.py
+++ csss-site/src/events/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     url(r'^board_games$', views.board_games, name='board_games'),
     url(r'^game_jam$', views.game_jam, name='game_jam'),
     url(r'^frosh_week$', views.frosh_week, name='froshWeek'),
+    url(r'^mountain_madness$', views.mountain_madness, name='mountain_madness'),
     url(r'^$', views.index, name='index'),
 ]
--- csss-site/src/events/views.py
+++ csss-site/src/events/views.py
@@ -65,4 +65,16 @@ def frosh_week(request):
     }
     return render(request, 'events/frosh_week.html', context)
 
+def mountain_madness(request):
+    groups = list(request.user.groups.values_list('name', flat=True))
+    context = {
+        'tab': 'events',
+        'authenticated': request.user.is_authenticated,
+        'Exec': ('Exec' in groups),
+        'ElectionOfficer': ('ElectionOfficer' in groups),
+        'Staff': request.user.is_staff,
+        'Username': request.user.username
+    }
+    return render(request, 'events/mountainmadness.github.io/index.html')
+
 # Create your views here.
