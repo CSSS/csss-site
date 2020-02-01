8f1e5fea880f5c19053298dcfd416045f7466fbf
Jace Manshadi <jaymanshadi@gmail.com>
2020-02-01 01:04:39 -0800

    updating the steps that are needed to be taken to deploy master to the prod server

---
+++ CI/deploy_changes.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+. ~/envCSSS/bin/activate
+cd ~/csss-site
+python3.7 -m pip install -r requirements.txt
+cd csss-site/src
+docker run --name csss_site_db -p 5432:5432 -it -d -e POSTGRES_PASSWORD=${POSTGRES_PASSWORD} postgres:alpine
+python3.7 manage.py migrate
+python3.7 manage.py makemigrations
+python3.7 manage.py migrate
+python3.7 manage.py makemigrations 750_project
+python3.7 manage.py migrate
+
+python3.7 manage.py makemigrations about
+python3.7 manage.py migrate
+
+python3.7 manage.py makemigrations administration
+python3.7 manage.py migrate
+
+python3.7 manage.py makemigrations announcements
+python3.7 manage.py migrate
+
+python3.7 manage.py makemigrations bursaries_and_awards
+python3.7 manage.py migrate
+
+python3.7 manage.py makemigrations comp_sci_guide
+python3.7 manage.py migrate
+
+python3.7 manage.py makemigrations documents
+python3.7 manage.py migrate
+
+python3.7 manage.py makemigrations elections
+python3.7 manage.py migrate
+
+python3.7 manage.py makemigrations events
+python3.7 manage.py migrate
+
+python3.7 manage.py makemigrations file_uploads
+python3.7 manage.py migrate
