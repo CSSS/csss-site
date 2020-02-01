55f815aa32d672227feaa4d527a5376fabf8ad09
VJ <vijenderbakhshi@gmail.com>
2020-01-31 21:37:26 -0800

    changed mm_hackthon/ -> mm_hackthon2020

--- csss-site/src/events/urls.py
+++ csss-site/src/events/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     url(r'^board_games$', views.board_games, name='board_games'),
     url(r'^game_jam$', views.game_jam, name='game_jam'),
     url(r'^frosh_week$', views.frosh_week, name='froshWeek'),
-    url(r'^mountain_madness2020$', views.mountain_madness2020, name='mountain_madness2020'),
+    url(r'^mountain_madness2020$', views.mountain_madness2020,
+        name='mountain_madness2020'),
     url(r'^$', views.index, name='index'),
 ]
