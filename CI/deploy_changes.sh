1d21c7ec5fc4ac25beb45be44153ebab3054a986
Jace Manshadi <jaymanshadi@gmail.com>
2020-04-12 22:50:55 -0700

    updating workflow so that the website now has a dev server for testing new features on so that merge approvers dont have to run changes from a PR on their own computer before accepting a pr

--- CI/deploy_changes.sh
+++ CI/deploy_changes.sh
@@ -2,26 +2,44 @@
 
 set -e -o xtrace
 
-. ~/envCSSS/bin/activate
-cd ~/csss-site
-python3.7 -m pip install -r requirements.txt
+function install_latest_python_requirements {
+  . ~/envCSSS/bin/activate
+  cd ~/csss-site
+  python3.8 -m pip install -r requirements.txt
+}
 
-chmod +x ~/setEnv.sh
-. ~/setEnv.sh site_envs
+function setup_website_db {
+  docker run --name csss_site_db -p ${DB_PORT}:5432 -it -d -e POSTGRES_PASSWORD=${DB_PASSWORD} postgres:alpine || true
+}
 
-docker run --name csss_site_db -p ${DB_PORT}:5432 -it -d -e POSTGRES_PASSWORD=${DB_PASSWORD} postgres:alpine || true
+function create_directory_for_website_logs {
+  mkdir -p ~/csss-site/csss-site/src/logs
+}
 
-mkdir -p ~/csss-site/csss-site/src/logs
+function applying_latest_db_migrations {
+  chmod +x ~/set_env.sh
+  . ~/set_env.sh site_envs
+  cd csss-site/src
+  chmod +x ~/migrate_apps.sh
+  . ~/migrate_apps.sh || true
+}
 
-cd csss-site/src
+function update_static_files_location {
+  # copying static files under their root directory
+  python3.8 manage.py collectstatic --noinput
 
-chmod +x ~/migrate_apps.sh
-. ~/migrate_apps.sh site_envs || true
+  # removing the static files that are under the source directory
+  find /home/csss/csss-site/csss-site/src -mindepth 1 -regex '.*static.*' -delete
+}
 
-python3.7 manage.py collectstatic --noinput
+function updating_gunincorn {
+  sudo systemctl restart gunicorn.service
+  sudo systemctl status gunicorn.service
+}
 
-find /home/csss/csss-site/csss-site/src -mindepth 1 -regex '.*static.*' -delete
-
-sudo systemctl restart gunicorn.service
-
-sudo systemctl status gunicorn.service
+install_latest_python_requirements
+setup_website_db
+create_directory_for_website_logs
+applying_latest_db_migrations
+update_static_files_location
+updating_gunincorn
---
+++ CI/update_files_on_server.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+set -e -o xtrace
+
+function remove_existing_files {
+  # remove all old and replace with newer code. will make sure that the migrations are not deleted as they need to be
+  # persistent through the changes
+  ssh csss@"${TARGET_SERVER}" \
+      "find /home/csss/csss-site/csss-site/src -mindepth 1 ! -regex '.*migrations.*' -delete" \
+      || true
+  ssh csss@"${TARGET_SERVER}" "rm /home/csss/csss-site/requirements.txt" || true
+  ssh csss@"${TARGET_SERVER}" "rm /home/csss/deploy_changes.sh" || true
+  ssh csss@"${TARGET_SERVER}" "rm /home/csss/site_envs" || true
+  ssh csss@"${TARGET_SERVER}" "rm /home/csss/set_env.sh" || true
+}
+
+function copy_source_code_and_reqs {
+  # create the folder that the source code for the website will go under
+  # and copy the source code to that folder
+  ssh csss@"${TARGET_SERVER}" "mkdir -p /home/csss/csss-site/csss-site/src"
+  scp -r "csss-site/src"/* csss@"${TARGET_SERVER}":/home/csss/csss-site/csss-site/src/
+
+  # transfer requirements file for the website
+  scp "requirements.txt" csss@"${TARGET_SERVER}":/home/csss/csss-site/requirements.txt
+}
+
+function transfer_env_variables_to_server {
+  # create the file that contains all the environment variables that
+  # site needs to run
+  echo "BASE_DIR=\"${BASE_DIR}\"" > site_envs
+  echo "WEBSITE_SECRET_KEY=\"${WEBSITE_SECRET_KEY}\"" >> site_envs
+  echo "DEBUG=\"${DEBUG}\"" >> site_envs
+  echo "HOST_ADDRESS=\"${HOST_ADDRESS}\"" >> site_envs
+  echo "DB_PASSWORD=\"${DB_PASSWORD}\"" >> site_envs
+  echo "STRIPE_PUBLISHABLE_KEY=\"${STRIPE_PUBLISHABLE_KEY}\"" >> site_envs
+  echo "STRIPE_SECRET_KEY=\"${STRIPE_SECRET_KEY}\"" >> site_envs
+  echo "DB_PORT=\"5432\"" >> site_envs
+  echo "DB_TYPE=\"postgres\"" >> site_envs
+  scp site_envs csss@"${TARGET_SERVER}":/home/csss/site_envs
+  scp "CI/set_env.sh" csss@"${TARGET_SERVER}":/home/csss/set_env.sh
+}
+
+function transfer_file_for_automating_app_migration {
+  scp CI/migrate_apps.sh csss@"${TARGET_SERVER}":/home/csss/migrate_apps.sh
+}
+
+function transfer_file_to_deploy_all_above_changes {
+  scp "CI/deploy_changes.sh" csss@"${TARGET_SERVER}":/home/csss/deploy_changes.sh
+}
+
+remove_existing_files
+copy_source_code_and_reqs
+transfer_env_variables_to_server
+transfer_file_for_automating_app_migration
+transfer_file_to_deploy_all_above_changes
