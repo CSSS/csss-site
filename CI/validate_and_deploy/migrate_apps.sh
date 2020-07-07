bce8f6de6a566fc63afbb8060dc4e68792fa8e84
Jace Manshadi <jaymanshadi@gmail.com>
2020-07-06 18:48:36 -0700

    adding yes flag to migrations can happen in CI

--- CI/validate_and_deploy/migrate_apps.sh
+++ CI/validate_and_deploy/migrate_apps.sh
@@ -3,13 +3,13 @@
 
 apps_to_migrate=($(ls -1 ./))
 python3 manage.py migrate
-python3 manage.py makemigrations
-python3 manage.py migrate
+yes | python3 manage.py makemigrations
+yes | python3 manage.py migrate
 
 for app_to_migrate in "${apps_to_migrate[@]}"
 do
    :
    echo "migrating ${app_to_migrate}"
-   python3 manage.py makemigrations ${app_to_migrate}
-   python3 manage.py migrate
+   yes | python3 manage.py makemigrations ${app_to_migrate}
+   yes | python3 manage.py migrate
 done
