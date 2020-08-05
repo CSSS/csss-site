f2777e957af55b7f5b979fc7676519f6da3e7a5f
Jace Manshadi <jaymanshadi@gmail.com>
2020-08-04 18:38:10 -0700

    adding the static and template files for 2011 just for historical purposes since 2011 was done with PHP and is not compatible with django

---
+++ csss-site/src/events/templates/events/frosh/2011/index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<title>CSSS FroshWeek 2011</title>
+<link rel="stylesheet" href="assets/css/screen.css" />
+</head>
+
+<body>
+
+<div class="header">
+
+	<div class="container">
+	
+		<div class="logo"></div>
+		
+		<div class="sponsors">
+			<p>Sponsored by:</p>
+			<img src="assets/images/header_sponsors.png" alt="FroshWeek Sponsors" />
+		</div>
+		
+
+		<ul class="navigation">
+			<li><a href="index.php">Home</a></li>
+			<li><a href="schedule.php">Schedule</a></li>
+			<li><a href="registration.php">Registration</a></li>
+			<li><a href="faq.php">F.A.Q.</a></li>
+			<li><a href="contact.php">Contact</a></li>
+			<li><a href="sponsors.php">Sponsors</a></li>
+		</ul>
+	
+	</div>
+
+</div>
+
+<div class="content">
+
+	<div class="container">
+<h1>What is FroshWeek?</h1>
+
+<img src="assets/images/tug-o-war.jpg" alt="Tug 'o War" />
+
+<p>FroshWeek is the ultimate introductory experience for Computing Science students at Simon Fraser
+University. Organized by the Computing Science Student Society, in partnership with the School of
+Computing Science, it is a week of fun-filled activities designed to orient, entertain and educate new
+university students with a Computing Science twist.</p>
+
+<p>Held during the first week of the Fall semester at SFU (Sept. 6 - 10), FroshWeek is organized by students, for
+students. Incoming students receive: an in-depth introduction to the facilities available to them as
+Computing Science students; many entertaining on- and off-campus challenges and diversions to
+motivate, amuse, and connect them with other students; valuable advice from their elders with respect 
+to courses and the university experience in general; and some free food and tools to make the
+transition to university much smoother.</p>
+
+<p>Now in its seventh year, Computing Science's Froshweek has matured into one of the most well
+received and recognized student-organized events at Simon Fraser University. It is a memorable and
+cherished week for participants and organizers alike.</p>
+
+<h1>What is the Computing Science Student Society?</h1>
+
+<p>The Computing Science Student Society is a departmental student society based out of Simon 
+Fraser University. Operating under the Simon Fraser Student Society, we act to recognize the
+academic and recreational demands of Computing Science students, providing them with a better
+experience at SFU. We have seats on the School of Computing Science Undergraduate Program 
+Committee and the Faculty of Applied Science Undergraduate Curriculum Committee. 
+The society also facilitates recreational activities, such as
+LAN parties, board game nights and movie nights, as well as joining
+the Simon Fraser Student Society and other departmental student
+societies for events. On a day-to-day level, the society provides
+services for Computing Science students such as a common area,
+printing, and inexpensive beverages.</p>
+
+	</div>
+
+</div>
+
+
+<div class="footer">
+	<div class="container">
+		<p>&copy; Copyright 2011 - <a href="http://csss.cs.sfu.ca/">Computing Science Student Society</a></p>
+	</div>
+</div>
+
+</body>
+</html>
