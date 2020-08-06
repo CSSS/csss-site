a96a4c54a310e4823d7f0d6a11027dbeb573237a
Jace Manshadi <jaymanshadi@gmail.com>
2020-08-05 21:57:40 -0700

    fixing template path

--- csss-site/src/static_pages/views.py
+++ csss-site/src/static_pages/views.py
@@ -6,8 +6,8 @@ TAB_STRING = 'more'
 
 
 def bursaries(request):
-    return render(request, 'static/bursaries_and_awards.html', create_main_context(request, TAB_STRING))
+    return render(request, 'static_pages/bursaries_and_awards.html', create_main_context(request, TAB_STRING))
 
 
 def guide(request):
-    return render(request, 'static/comp_sci_guide.html', create_main_context(request, TAB_STRING))
+    return render(request, 'static_pages/comp_sci_guide.html', create_main_context(request, TAB_STRING))
