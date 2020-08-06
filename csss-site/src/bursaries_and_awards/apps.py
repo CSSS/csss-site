e3324aa6b558923d8c1b29d3fbedef82fe2a7f78
Jace Manshadi <jaymanshadi@gmail.com>
2020-08-05 19:18:10 -0700

    combining static pages into a single app to cut down on unnecessary apps

--- csss-site/src/bursaries_and_awards/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class BursariesAndAwardsConfig(AppConfig):
-    name = 'Bursaries and Awards'
--- csss-site/src/comp_sci_guide/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-from csss.views_helper import create_main_context
-
-
-def software(request):
-    return render(request, 'comp_sci_guide/software.html', create_main_context(request, "comp_sci_guide"))
