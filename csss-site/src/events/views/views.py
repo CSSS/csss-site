46ce91950c5f7429d0cf08183a06b21109b15c8a
Jace Manshadi <jaymanshadi@gmail.com>
2020-08-06 20:15:19 -0700

    removing outdated functions

--- csss-site/src/events/views/views.py
+++ csss-site/src/events/views/views.py
@@ -35,12 +35,3 @@ def frosh_2015(request):
 
 def mountain_madness2020(request):
     return render(request, 'events/mountain_madness2020.html', create_main_context(request, TAB))
-
-
-# Create your views here.
-def frosh_2015_v2(request):
-    return render(request, 'events/frosh/2015_v2/index.html', create_main_context(request, TAB))
-
-
-def frosh_2015_temp(request):
-    return render(request, 'events/frosh/2015_temp_site/index.html', create_main_context(request, TAB))
