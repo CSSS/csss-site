b32ae47c156bb34d1943dac7755d6bb6e2b62126
John Wu <john_wu966@hotmail.com>
2020-08-24 18:14:34 -0700

    Frosh 2020 Changes

--- csss-site/src/events/urls.py
+++ csss-site/src/events/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 
 from .views import views
-from .views.frosh import frosh2012, frosh2013, frosh2014, frosh2015, frosh2016, frosh2017, frosh2018, frosh2019
+from .views.frosh import frosh2012, frosh2013, frosh2014, frosh2015, frosh2016, frosh2017, frosh2018, frosh2019,frosh2020
 
 urlpatterns = [
     url(r'^general_meetings$', views.gm, name='gm'),
@@ -41,5 +41,7 @@ urlpatterns = [
     url(r'^frosh/2018/conditions$', frosh2018.conditions, name="Frosh Week 2018 Conditions"),
     url(r'^frosh/2019$', frosh2019.index, name="Frosh Week 2019"),
     url(r'^frosh/2019/frosh$', frosh2019.frosh, name="Frosh Week 2019 Frosh"),
+    url(r'^frosh/2020$', frosh2020.index, name="Frosh Week 2020"),
+    url(r'^frosh/2020/frosh$', frosh2020.frosh, name="Frosh Week 2020 Frosh"),
     url(r'^$', views.index, name='index'),
 ]
