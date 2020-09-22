521c9a5243e4c82dbad36a1ac3236c4d9f690479
Jace Manshadi <jaymanshadi@gmail.com>
2020-09-22 00:52:28 -0700

    preventing code from trying to remove csss from its own group

--- csss-site/src/resource_management/views/resource_apis/gitlab/gitlab_api.py
+++ csss-site/src/resource_management/views/resource_apis/gitlab/gitlab_api.py
@@ -147,7 +147,7 @@ class GitLabAPI:
                 )
                 return False, "Unable to get the CSSS SFU Gitlab group"
             for membership in csss_group.members.list(all=True):
-                if len([
+                if membership.username != "csss1" and len([
                     user for user in users if user == membership.username
                 ]) == 0:
                     self.remove_user_from_group([membership.username])
