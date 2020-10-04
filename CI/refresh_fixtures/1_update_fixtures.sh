a74b9487df28dcdc345c3f80e69aadf956214bef
Jace Manshadi <jaymanshadi@gmail.com>
2020-10-04 14:08:18 -0700

    updating script so that it uses jq for santizing sensitive info from officer bios

--- CI/refresh_fixtures/1_update_fixtures.sh
+++ CI/refresh_fixtures/1_update_fixtures.sh
@@ -15,7 +15,12 @@ ssh csss@sfucsss.org "rm -fr /home/csss/csss-site/csss-site/src/*.json"
 ssh csss@sfucsss.org "rm /home/csss/create_jsons.sh"
 
 mv about.json csss-site/src/about/fixtures/.
-python CI/refresh_fixtures/sanitize_officer_info.py
+# ugly blob of jq obtained from https://stackoverflow.com/a/64181110/7734535
+# just aiming to cleans the bios from any sensitive info about the officers so their information
+# is not leaked in the staging environnment
+jq ' map( reduce ("sfuid", "sfu_email_alias", "github_username", "gmail") as $k (.; if .fields[$k] then .fields[$k] = $k else . end) | if .fields.phone_number then .fields.phone_number = 0 else . end )' \
+csss-site/src/about/fixtures/about.json  > csss-site/src/about/fixtures/temp_about.json
+mv csss-site/src/about/fixtures/temp_about.json csss-site/src/about/fixtures/about.json
 mv announcements.json csss-site/src/announcements/fixtures/.
 mv django_mailbox.json csss-site/src/announcements/fixtures/.
 mv elections.json csss-site/src/elections/fixtures/.
