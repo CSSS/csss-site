8b7e27fac90c042d813fc94b317e472f70ee0535
Jace Manshadi <jmanshad@protonmail.com>
2020-10-08 20:17:30 -0700

    correcting env file name

--- CI/validate_and_deploy/2_deploy/2_deploy_changes.sh
+++ CI/validate_and_deploy/2_deploy/2_deploy_changes.sh
@@ -62,7 +62,7 @@ function setup_website_db {
 
 function applying_latest_db_migrations {
   chmod +x set_env.sh
-  . ./set_env.sh site_envs
+  . ./set_env.sh site_envs_django_admin
 
   setup_website_db
 
