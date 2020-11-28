b48e1d851e0883fdcdceab442d07d774722e1a5d
Jace Manshadi <jmanshad@protonmail.com>
2020-11-28 15:59:58 -0800

    adding missing double quotes

--- CI/backup_db/jenkins_backup_script.sh
+++ CI/backup_db/jenkins_backup_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-scp CI/backup_db/backup.sh csss@"${HOST_ADDRESS}":"${BASE_DIR}/backup.sh
+scp CI/backup_db/backup.sh csss@"${HOST_ADDRESS}":"${BASE_DIR}/backup.sh"
 ssh csss@"\${HOST_ADDRESS}" "${BASE_DIR}/backup.sh";
 ssh csss@"\${HOST_ADDRESS}" "rm ${BASE_DIR}/backup.sh";
