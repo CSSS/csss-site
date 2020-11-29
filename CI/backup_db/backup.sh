e5495d228fbfa40d10245499ce7bf58dad1c5056
Jace Manshadi <jmanshad@protonmail.com>
2020-11-28 16:11:07 -0800

    updating script so that json is properly indented

--- CI/backup_db/backup.sh
+++ CI/backup_db/backup.sh
@@ -4,6 +4,8 @@ cd ~/
 . set_env.sh site_envs_django_admin
 . envCSSS/bin/activate
 cd csss-site
-python3.8 manage.py dumpdata --output website_prod_backup.json
+python3.8 manage.py dumpdata --output temp_website_prod_backup.json
+jq . temp_website_prod_backup.json > website_prod_backup.json
+rm temp_website_prod_backup.json
 scp website_prod_backup.json  csss@fraser.sfu.ca:/home/csss/.
 rm website_prod_backup.json
