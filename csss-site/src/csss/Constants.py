359217f48fbe699505f13e93254df7b3febcc543
Jace Manshadi <jmanshad@protonmail.com>
2020-12-09 18:31:06 -0800

    adding constants class

---
+++ csss-site/src/csss/Constants.py
@@ -0,0 +1,8 @@
+class Constants:
+    position_name = "position_name"
+    position_index = "position_index"
+    position_email = "position_email"
+    user_select_to_a_position_mapping_option = "action"
+    user_select_to_update_position_mapping = "update"
+    user_select_to_delete_position_mapping = "delete"
+    user_select_to_un_delete_position_mapping = "un_delete"
