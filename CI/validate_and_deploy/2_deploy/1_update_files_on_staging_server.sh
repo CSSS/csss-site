d3ec94070a82384e8474769dd7569330a461b2a4
Jace Manshadi <jmanshad@protonmail.com>
2020-12-19 18:26:36 -0800

    updating how the staging and master branch's are launched (#245)
    * adding a new script just for launching staging version of website
    
    * creating the home directory for the staging branch
    
    * switchig order around since the variable BASE_DIR is needed in order to create log files
    
    * fixing path for requirements
    
    * removing redundant loaddata commands
    
    * updating the workflow to be different for staging vs master branch
    
    * having the CI for staging use the script for updating the fixtures on the go

--- CI/validate_and_deploy/2_deploy/1_update_files_on_staging_server.sh
+++ CI/validate_and_deploy/2_deploy/1_update_files_on_staging_server.sh
@@ -2,29 +2,15 @@
 
 set -e -o xtrace
 
+function create_staging_branch_home_dir {
+  ssh csss@"${HOST_ADDRESS}" "mkdir -p ${BASE_DIR}" || true
+}
+
 function remove_existing_files {
-  # remove all old and replace with newer code. will make sure that the migrations are not deleted as they need to be
-  # persistent through the changes
-  ssh csss@"${HOST_ADDRESS}" \
-      "rm -fr ${BASE_DIR}/csss-site" \
-      || true
-  ssh csss@"${HOST_ADDRESS}" "rm ${BASE_DIR}/deploy_changes.sh" || true
   ssh csss@"${HOST_ADDRESS}" "rm ${BASE_DIR}/site_envs" || true
   ssh csss@"${HOST_ADDRESS}" "rm ${BASE_DIR}/site_envs_django_admin" || true
   ssh csss@"${HOST_ADDRESS}" "rm ${BASE_DIR}/site_envs_gunicorn" || true
-  ssh csss@"${HOST_ADDRESS}" "rm ${BASE_DIR}/set_env.sh" || true
-  ssh csss@"${HOST_ADDRESS}" "rm ${BASE_DIR}/requirements.txt" || true
 
-}
-
-function transfer_source_code_and_reqs {
-  # create the folder that the source code for the website will go under
-  # and copy the source code to that folder
-  ssh csss@"${HOST_ADDRESS}" "mkdir -p ${BASE_DIR}/csss-site"
-  scp -r "csss-site/src"/* csss@"${HOST_ADDRESS}":"${BASE_DIR}/csss-site/"
-
-  # transfer requirements file for the website
-  scp "requirements.txt" csss@"${HOST_ADDRESS}":"${BASE_DIR}/requirements.txt"
 }
 
 function transfer_env_variables_to_server {
@@ -47,24 +33,20 @@ function transfer_env_variables_to_server {
   echo 'GITLAB_PRIVATE_TOKEN='"'"${GITLAB_PRIVATE_TOKEN}"'" >> site_envs
   echo 'LOG_LOCATION='"'"${BASE_DIR}/website_logs/python_logs"'" > site_envs_django_admin
   echo 'LOG_LOCATION='"'"${BASE_DIR}/website_logs/gunicorn_logs"'" > site_envs_gunicorn
+  echo 'DB_NAME='"'"${BRANCH_NAME}"'" >> site_envs
+  echo 'CHANGE_ID='"'"${CHANGE_ID}"'" >> site_envs
 
-  if [[ "${BRANCH_NAME}" != "master" ]]; then
-    echo 'DB_NAME='"'"${BRANCH_NAME}"'" >> site_envs
-  else
-    echo 'DB_NAME='"'postgres'" >> site_envs
-  fi
   cat site_envs >> site_envs_django_admin
   cat site_envs >>  site_envs_gunicorn
   scp site_envs_django_admin csss@"${HOST_ADDRESS}":"${BASE_DIR}/site_envs_django_admin"
   scp site_envs_gunicorn csss@"${HOST_ADDRESS}":"${BASE_DIR}/site_envs_gunicorn"
-  scp "CI/validate_and_deploy/2_deploy/set_env.sh" csss@"${HOST_ADDRESS}":"${BASE_DIR}/set_env.sh"
 }
 
 function transfer_file_to_deploy_all_above_changes {
-  scp "CI/validate_and_deploy/2_deploy/2_deploy_changes.sh" csss@"${HOST_ADDRESS}":"${BASE_DIR}/deploy_changes.sh"
+  scp "CI/validate_and_deploy/2_deploy/2_deploy_staging_changes.sh" csss@"${HOST_ADDRESS}":"${BASE_DIR}/deploy_changes.sh"
 }
 
+create_staging_branch_home_dir
 remove_existing_files
-transfer_source_code_and_reqs
 transfer_env_variables_to_server
 transfer_file_to_deploy_all_above_changes
---
+++ CI/validate_and_deploy/2_deploy/2_deploy_master_changes.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+set -e -o xtrace
+
+function go_to_root_directory {
+  BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+  cd "${BASE_DIR}"
+}
+
+function install_latest_python_requirements {
+  rm -fr "${BASE_DIR}/envCSSS"
+  python3 -m virtualenv envCSSS
+  . "${BASE_DIR}/envCSSS/bin/activate"
+  python3 -m pip install -r "${BASE_DIR}/requirements.txt"
+}
+
+function create_directory_for_website_logs {
+  mkdir -p "${BASE_DIR}/website_logs/python_logs"
+  mkdir -p "${BASE_DIR}/website_logs/gunicorn_logs"
+}
+
+function wait_for_postgres_db {
+  # aquired from https://docs.docker.com/compose/startup-order/
+  until PGPASSWORD=$DB_PASSWORD psql -h localhost -p "${DB_PORT}" -U "postgres" -c '\q'; do
+    >&2 echo "Postgres is unavailable - sleeping"
+    sleep 1
+  done
+
+  >&2 echo "Postgres is up"
+}
+
+function setup_website_db {
+  export DB_CONTAINER_NAME="csss_site_db"
+  docker run --name "${DB_CONTAINER_NAME}" -p "${DB_PORT}":5432 -it -d -e POSTGRES_PASSWORD="${DB_PASSWORD}" postgres:alpine || true
+  wait_for_postgres_db
+  docker exec "${DB_CONTAINER_NAME}" psql -U postgres -d postgres -c "CREATE DATABASE \"${DB_NAME}\" OWNER postgres;" || true
+}
+
+function applying_latest_db_migrations {
+  chmod +x set_env.sh
+  . ./set_env.sh site_envs_django_admin
+
+  setup_website_db
+
+  cd "${BASE_DIR}/csss-site"
+  python3 manage.py migrate
+}
+
+function update_static_files_location {
+  # copying static files under their root directory
+  python3 manage.py collectstatic --noinput
+
+  # removing the static files that are under the source directory
+  find "${BASE_DIR}/csss-site" -mindepth 1 -name 'static' -exec rm -rv {} +
+}
+
+function set_gunicorn_files {
+  sudo systemctl daemon-reload
+}
+
+function updating_gunincorn {
+  gunicorn_socket="gunicorn.socket"
+  gunicorn="gunicorn.service"
+  sudo systemctl restart "${gunicorn_socket}"
+  sudo systemctl enable "${gunicorn_socket}"
+  sudo systemctl status "${gunicorn_socket}"
+  file "${socket_file_location}"
+  sudo journalctl -u "${gunicorn_socket}"
+
+  sudo systemctl restart "${gunicorn}"
+  sudo systemctl enable "${gunicorn}"
+  sudo systemctl status "${gunicorn}"
+}
+
+function update_nginx_configuration {
+  sudo systemctl restart nginx
+}
+
+function clean_up_after_deployment {
+  rm "/home/csss/deploy_changes.sh"
+}
+
+go_to_root_directory
+install_latest_python_requirements
+create_directory_for_website_logs
+applying_latest_db_migrations
+update_static_files_location
+set_gunicorn_files
+updating_gunincorn
+update_nginx_configuration
+clean_up_after_deployment
