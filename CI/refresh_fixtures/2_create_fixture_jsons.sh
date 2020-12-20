c96c70c849580f6dc87f66faed929c434f6d49e8
Jace Manshadi <jmanshad@protonmail.com>
2020-12-19 19:22:00 -0800

    removing minor version since prod uses python3.6

--- CI/refresh_fixtures/2_create_fixture_jsons.sh
+++ CI/refresh_fixtures/2_create_fixture_jsons.sh
@@ -6,8 +6,8 @@ set -e -o xtrace
 . ~/envCSSS/bin/activate
 cd ~/csss-site/
 
-python3.8 manage.py dumpdata about --indent 4 --output about.json
-python3.8 manage.py dumpdata announcements --indent 4 --output announcements.json
-python3.8 manage.py dumpdata django_mailbox --indent 4 --output django_mailbox.json
-python3.8 manage.py dumpdata elections --indent 4 --output elections.json
-python3.8 manage.py dumpdata resource_management.officergithubteam --indent 4 --output resource_management.json
+python3 manage.py dumpdata about --indent 4 --output about.json
+python3 manage.py dumpdata announcements --indent 4 --output announcements.json
+python3 manage.py dumpdata django_mailbox --indent 4 --output django_mailbox.json
+python3 manage.py dumpdata elections --indent 4 --output elections.json
+python3 manage.py dumpdata resource_management.officergithubteam --indent 4 --output resource_management.json
