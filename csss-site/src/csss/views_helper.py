0b3b79c88c5c8752182aa289430f2226b1b8210f
Jace Manshadi <jmanshad@protonmail.com>
2021-01-19 17:32:52 -0800

    code and comments updated and cleaned

--- csss-site/src/csss/views_helper.py
+++ csss-site/src/csss/views_helper.py
@@ -44,6 +44,12 @@ def create_main_context(request, tab, groups=None):
 
 
 def create_frosh_context():
+    """
+    creates the context dictionary for the frosh webpages
+
+    Return
+    context -- the frosh webpages context dictionary
+    """
     return _create_base_context()
 
 
@@ -62,6 +68,19 @@ def _create_base_context():
 
 
 def verify_access_logged_user_and_create_context_for_elections(request, tab):
+    """
+    Makes sure that the user is allowed to access the election page and returns
+    the context dictionary
+
+    Keyword Argument
+    request -- the django request object
+    tab -- the tab that needs to be specified in the context
+
+    Return
+    HttpResponseRedirect -- either None or the redirect object that redirect to the error page
+    error_message -- the error message if the user is not allowed to access the election pages
+    context -- the base context dictionary
+    """
     groups = list(request.user.groups.values_list('name', flat=True))
     context = create_main_context(request, tab, groups)
     if not ('election_officer' in groups or request.user.is_staff):
@@ -72,14 +91,14 @@ def verify_access_logged_user_and_create_context_for_elections(request, tab):
 
 def verify_access_logged_user_and_create_context(request, tab):
     """
-    make sure that the user is logged in witth the sufficient level of access to access the page
+    make sure that the user is logged in with the sufficient level of access to access the page
 
     Keyword Arguments
     request -- the django request object
     tab -- the tab that needs to be specified in the context
 
     Return
-    http redirect -- returns a redirect to /error if the user is not allowed to access the page or None if they are
+    HttpResponseRedirect -- returns a redirect to /error if the user is not allowed to access the page or None if they are
     error_message -- the error message if the user is not allowed to access the page
     context -- the context object to pass to html if user is allowed to access the page
     """
