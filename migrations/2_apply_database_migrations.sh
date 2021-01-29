3ad6e80f9abe0aea0be5c40c872fe844d18a729d
Jace Manshadi <jmanshad@protonmail.com>
2021-01-28 18:46:23 -0800

    fixing name used for docker db

--- migrations/2_apply_database_migrations.sh
+++ migrations/2_apply_database_migrations.sh
@@ -6,9 +6,10 @@ git checkout migration_CI
 
 docker stop csss_website_db || true
 docker rm csss_website_db || true
-docker run --name ${DB_NAME} -p ${DB_PORT}:5432 -it -d -e POSTGRES_PASSWORD=${DB_PASSWORD} postgres:alpine
+export DB_CONTAINER_NAME="csss_site_db_dev"
+docker run --name ${DB_CONTAINER_NAME} -p ${DB_PORT}:5432 -it -d -e POSTGRES_PASSWORD=${DB_PASSWORD} postgres:alpine
 sleep 5
-docker exec "${DB_NAME}" psql -U postgres -d postgres -c "CREATE DATABASE \"${DB_NAME}\" OWNER postgres;" || true
+docker exec "${DB_CONTAINER_NAME}" psql -U postgres -d postgres -c "CREATE DATABASE \"${DB_NAME}\" OWNER postgres;" || true
 
 python3 manage.py migrate
 python3 manage.py loaddata ../../migrations/fixtures/*
