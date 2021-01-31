30b39fc797f9693b202c481fd0b114d5f91e0b7e
SpartanApple <mgale@sfu.ca>
2021-01-30 20:54:19 -0800

    Added Fall_Hacks 2020 webpage and Mountain Madness 2021 site

--- csss-site/src/events/views/views.py
+++ csss-site/src/events/views/views.py
@@ -35,3 +35,9 @@ def frosh_2015(request):
 
 def mountain_madness2020(request):
     return render(request, 'events/mountain_madness2020.html', create_main_context(request, TAB))
+
+def mountain_madness2021(request):
+    return render(request, 'events/mountain_madness2021.html', create_main_context(request, TAB))
+
+def fall_hacks2020(request):
+    return render(request, 'events/fall_hacks2020.html', create_main_context(request, TAB))
