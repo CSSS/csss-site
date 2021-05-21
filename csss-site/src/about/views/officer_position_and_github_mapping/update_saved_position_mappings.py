97c4611a55368fae8af931c6d58f5d9f23e8c4c2
Jace Manshadi <jmanshad@protonmail.com>
2021-05-20 19:34:44 -0700

    updating code to fix flake8 issues

--- csss-site/src/about/views/officer_position_and_github_mapping/update_saved_position_mappings.py
+++ csss-site/src/about/views/officer_position_and_github_mapping/update_saved_position_mappings.py
@@ -28,115 +28,130 @@ def update_saved_position_mappings(request):
         return render_value
 
     if request.method == "POST":
-        post_dict = parser.parse(request.POST.urlencode())
-        for position in list(post_dict['saved_officer_positions'].values()):
-            _update_position_mapping(position)
+        context[ERROR_MESSAGES_KEY] = _update_positions_mapping(
+            list(
+                parser.parse(
+                    request.POST.urlencode()
+                )['saved_officer_positions'].values()
+            )
+        )
     return render(request, 'about/position_mapping/position_mapping.html', update_context(context))
 
 
-def _update_position_mapping(post_dict):
+def _update_positions_mapping(positions):
     """
     Updates the position mapping for the specified position
 
     Keyword Argument
-    post_dict -- request.POST in dictionary object
+    position -- the dict for a specific position
 
     Return
     error_messages -- a list of all the possible error messages
     """
-    if not (OFFICER_EMAIL_LIST_AND_POSITION_MAPPING__ID in post_dict
-            and f"{post_dict[OFFICER_EMAIL_LIST_AND_POSITION_MAPPING__ID]}".isdigit()
-            and len(
-                OfficerEmailListAndPositionMapping.objects.all().filter(
-                    id=int(post_dict[OFFICER_EMAIL_LIST_AND_POSITION_MAPPING__ID])
-                )
-            ) > 0):
-        error_message = "No valid position mapping id detected"
-        logger.info(f"[about/update_saved_position_mappings.py _update_position_mapping()] {error_message}")
-        return [error_message]
+    for position in positions:
+        if not (OFFICER_EMAIL_LIST_AND_POSITION_MAPPING__ID in position
+                and f"{position[OFFICER_EMAIL_LIST_AND_POSITION_MAPPING__ID]}".isdigit()
+                and len(
+                    OfficerEmailListAndPositionMapping.objects.all().filter(
+                        id=int(position[OFFICER_EMAIL_LIST_AND_POSITION_MAPPING__ID])
+                    )
+                ) > 0):
+            error_message = "No valid position mapping id detected"
+            logger.info(f"[about/update_saved_position_mappings.py _update_position_mapping()] {error_message}")
+            return [error_message]
+        if not (OFFICER_EMAIL_LIST_AND_POSITION_MAPPING__POSITION_INDEX in position
+                and f"{position[OFFICER_EMAIL_LIST_AND_POSITION_MAPPING__POSITION_INDEX]}".isdigit()):
+            error_message = "No valid position index detected for position mapping"
+            logger.info(f"[about/update_saved_position_mappings.py _update_position_mapping()] {error_message}")
+            return [error_message]
+        if not (OFFICER_EMAIL_LIST_AND_POSITION_MAPPING__POSITION_NAME in position):
+            error_message = "No valid position name detected for position mapping"
+            logger.info(f"[about/update_saved_position_mappings.py _update_position_mapping()] {error_message}")
+            return [error_message]
+        if not (OFFICER_EMAIL_LIST_AND_POSITION_MAPPING__EMAIL_LIST_ADDRESS in position):
+            error_message = "No valid position email list detected for position mapping"
+            logger.info(f"[about/update_saved_position_mappings.py _update_position_mapping()] {error_message}")
+            return [error_message]
+        if not (OFFICER_EMAIL_LIST_AND_POSITION_MAPPING__ELECTION_POSITION in position):
+            error_message = "No valid position elected status detected for position mapping"
+            logger.info(f"[about/update_saved_position_mappings.py _update_position_mapping()] {error_message}")
+            return [error_message]
 
-    if not (OFFICER_EMAIL_LIST_AND_POSITION_MAPPING__POSITION_INDEX in post_dict
-            and f"{post_dict[OFFICER_EMAIL_LIST_AND_POSITION_MAPPING__POSITION_INDEX]}".isdigit()):
-        error_message = "No valid position index detected for position mapping"
-        logger.info(f"[about/update_saved_position_mappings.py _update_position_mapping()] {error_message}")
-        return [error_message]
-    if not (OFFICER_EMAIL_LIST_AND_POSITION_MAPPING__POSITION_NAME in post_dict):
-        error_message = "No valid position name detected for position mapping"
-        logger.info(f"[about/update_saved_position_mappings.py _update_position_mapping()] {error_message}")
-        return [error_message]
-    if not (OFFICER_EMAIL_LIST_AND_POSITION_MAPPING__EMAIL_LIST_ADDRESS in post_dict):
-        error_message = "No valid position email list detected for position mapping"
-        logger.info(f"[about/update_saved_position_mappings.py _update_position_mapping()] {error_message}")
-        return [error_message]
-    if not (OFFICER_EMAIL_LIST_AND_POSITION_MAPPING__ELECTION_POSITION in post_dict):
-        error_message = "No valid position elected status detected for position mapping"
-        logger.info(f"[about/update_saved_position_mappings.py _update_position_mapping()] {error_message}")
-        return [error_message]
-
-    position_mapping_for_selected_officer = OfficerEmailListAndPositionMapping.objects.get(
-        id=int(post_dict[OFFICER_EMAIL_LIST_AND_POSITION_MAPPING__ID])
-    )
-    logger.info(
-        f"[about/update_saved_position_mappings.py _update_position_mapping()] "
-        f"user has selected to update the position {position_mapping_for_selected_officer.position_name}"
-    )
-
-    new_position_index_for_officer_position = int(post_dict[OFFICER_EMAIL_LIST_AND_POSITION_MAPPING__POSITION_INDEX])
-    new_name_for_officer_position = post_dict[OFFICER_EMAIL_LIST_AND_POSITION_MAPPING__POSITION_NAME]
-    new_sfu_email_list_address_for_officer_position = \
-        post_dict[OFFICER_EMAIL_LIST_AND_POSITION_MAPPING__EMAIL_LIST_ADDRESS]
-    elected_via_election_officer = \
-        post_dict[OFFICER_EMAIL_LIST_AND_POSITION_MAPPING__ELECTION_POSITION]
-
-    if new_name_for_officer_position == position_mapping_for_selected_officer.position_name \
-            and new_position_index_for_officer_position == position_mapping_for_selected_officer.position_index \
-            and new_sfu_email_list_address_for_officer_position == position_mapping_for_selected_officer.email \
-            and elected_via_election_officer == position_mapping_for_selected_officer.elected_via_election_officer:
-        return []
-
-    logger.info(
-        f"[about/update_saved_position_mappings.py _update_position_mapping()] the user's "
-        f"change to the position {position_mapping_for_selected_officer.position_name} was detected"
-    )
-    # if anything has been changed for the selected position
-    success = True
-    error_message = None
-    previous_position_index = position_mapping_for_selected_officer.position_index
-    previous_position_name = position_mapping_for_selected_officer.position_name
-    if new_position_index_for_officer_position != previous_position_index:
-        success, error_message = validate_position_index(new_position_index_for_officer_position)
-    if success and new_name_for_officer_position != previous_position_name:
-        success, error_message = validate_position_name(new_name_for_officer_position)
-
-    if success:
-        terms = Term.objects.all().filter(term_number=get_current_term())
-        if len(terms) > 0:
-            term = terms[0]
-            officers_in_current_term_that_need_update = Officer.objects.all().filter(
-                elected_term=term,
-                position_index=position_mapping_for_selected_officer.position_index
-            )
-            logger.info(
-                f"[about/update_saved_position_mappings.py _update_position_mapping()] updating"
-                f" {len(officers_in_current_term_that_need_update)} officers due to change in position"
-                f" {position_mapping_for_selected_officer.position_name}"
-            )
-            for officer_in_current_term_that_need_update in officers_in_current_term_that_need_update:
-                officer_in_current_term_that_need_update.position_index = new_position_index_for_officer_position
-                officer_in_current_term_that_need_update.sfu_officer_mailing_list_email = \
-                    new_sfu_email_list_address_for_officer_position
-                officer_in_current_term_that_need_update.position_name = new_name_for_officer_position
-                officer_in_current_term_that_need_update.save()
-        position_mapping_for_selected_officer.position_name = new_name_for_officer_position
-        position_mapping_for_selected_officer.position_index = new_position_index_for_officer_position
-        position_mapping_for_selected_officer.email = new_sfu_email_list_address_for_officer_position
-        position_mapping_for_selected_officer.elected_via_election_officer = elected_via_election_officer
-        position_mapping_for_selected_officer.save()
-    else:
+        position_mapping_for_selected_officer = OfficerEmailListAndPositionMapping.objects.get(
+            id=int(position[OFFICER_EMAIL_LIST_AND_POSITION_MAPPING__ID])
+        )
         logger.info(
-            "[about/update_saved_position_mappings.py _update_position_mapping()]"
-            f" encountered error {error_message} when trying to update position"
-            f" {position_mapping_for_selected_officer.position_name}"
+            f"[about/update_saved_position_mappings.py _update_position_mapping()] "
+            f"user has selected to update the position {position_mapping_for_selected_officer.position_name}"
         )
 
-    return [error_message]
+        new_position_index_for_officer_position = int(
+            position[OFFICER_EMAIL_LIST_AND_POSITION_MAPPING__POSITION_INDEX]
+        )
+        new_name_for_officer_position = position[OFFICER_EMAIL_LIST_AND_POSITION_MAPPING__POSITION_NAME]
+        new_sfu_email_list_address_for_officer_position = \
+            position[OFFICER_EMAIL_LIST_AND_POSITION_MAPPING__EMAIL_LIST_ADDRESS]
+        elected_via_election_officer = \
+            position[OFFICER_EMAIL_LIST_AND_POSITION_MAPPING__ELECTION_POSITION]
+
+        if officer_info_is_not_changed(position_mapping_for_selected_officer, new_name_for_officer_position,
+                                       new_position_index_for_officer_position,
+                                       new_sfu_email_list_address_for_officer_position,
+                                       elected_via_election_officer):
+            continue
+        logger.info(
+            f"[about/update_saved_position_mappings.py _update_position_mapping()] the user's "
+            f"change to the position {position_mapping_for_selected_officer.position_name} was detected"
+        )
+        # if anything has been changed for the selected position
+        success = True
+        error_message = None
+        previous_position_index = position_mapping_for_selected_officer.position_index
+        previous_position_name = position_mapping_for_selected_officer.position_name
+        if new_position_index_for_officer_position != previous_position_index:
+            success, error_message = validate_position_index(new_position_index_for_officer_position)
+        if success and new_name_for_officer_position != previous_position_name:
+            success, error_message = validate_position_name(new_name_for_officer_position)
+
+        if success:
+            terms = Term.objects.all().filter(term_number=get_current_term())
+            if len(terms) > 0:
+                term = terms[0]
+                officers_in_current_term_that_need_update = Officer.objects.all().filter(
+                    elected_term=term,
+                    position_index=position_mapping_for_selected_officer.position_index
+                )
+                logger.info(
+                    f"[about/update_saved_position_mappings.py _update_position_mapping()] updating"
+                    f" {len(officers_in_current_term_that_need_update)} officers due to change in position"
+                    f" {position_mapping_for_selected_officer.position_name}"
+                )
+                for officer_in_current_term_that_need_update in officers_in_current_term_that_need_update:
+                    officer_in_current_term_that_need_update.position_index = new_position_index_for_officer_position
+                    officer_in_current_term_that_need_update.sfu_officer_mailing_list_email = \
+                        new_sfu_email_list_address_for_officer_position
+                    officer_in_current_term_that_need_update.position_name = new_name_for_officer_position
+                    officer_in_current_term_that_need_update.save()
+            position_mapping_for_selected_officer.position_name = new_name_for_officer_position
+            position_mapping_for_selected_officer.position_index = new_position_index_for_officer_position
+            position_mapping_for_selected_officer.email = new_sfu_email_list_address_for_officer_position
+            position_mapping_for_selected_officer.elected_via_election_officer = elected_via_election_officer
+            position_mapping_for_selected_officer.save()
+        else:
+            logger.info(
+                "[about/update_saved_position_mappings.py _update_position_mapping()]"
+                f" encountered error {error_message} when trying to update position"
+                f" {position_mapping_for_selected_officer.position_name}"
+            )
+
+        return [error_message]
+
+
+def officer_info_is_not_changed(position_mapping_for_selected_officer, new_name_for_officer_position,
+                                new_position_index_for_officer_position,
+                                new_sfu_email_list_address_for_officer_position,
+                                elected_via_election_officer):
+    return new_name_for_officer_position == position_mapping_for_selected_officer.position_name \
+           and new_position_index_for_officer_position == position_mapping_for_selected_officer.position_index \
+           and new_sfu_email_list_address_for_officer_position == position_mapping_for_selected_officer.email \
+           and elected_via_election_officer == position_mapping_for_selected_officer.elected_via_election_officer
