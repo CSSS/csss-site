1c2cd09123895f4ef6d6923c582b4cf66db738e2
Jace Manshadi <jmanshad@protonmail.com>
2021-05-24 21:06:32 -0700

    fixing flake8 issue

--- csss-site/src/events/views/views.py
+++ csss-site/src/events/views/views.py
@@ -4,6 +4,7 @@ from csss.views_helper import create_main_context
 
 TAB = 'events'
 
+
 def regular_events(request):
     return render(request, 'events/regular_events.html', create_main_context(request, TAB))
 
