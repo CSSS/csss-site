e630a796de39becfb00ab0c65da1697c6022e78c
Jace Manshadi <jmanshad@protonmail.com>
2021-05-30 00:58:39 -0700

    adding the mm page for 2019 from https://github.com/mountainmadness/mountainmadness.github.io

--- csss-site/src/events/urls/mm.py
+++ csss-site/src/events/urls/mm.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from events.views import mm
 
 urlpatterns = [
+    url(r'^mountain_madness2019$', mm.mountain_madness2019, name='mountain_madness2019'),
     url(r'^mountain_madness2020$', mm.mountain_madness2020, name='mountain_madness2020'),
     url(r'^mountain_madness2021$', mm.mountain_madness2021, name='mountain_madness2021'),
 ]
--- csss-site/src/events/views/mm.py
+++ csss-site/src/events/views/mm.py
@@ -5,6 +5,10 @@ from csss.views_helper import create_main_context
 TAB = 'events'
 
 
+def mountain_madness2019(request):
+    return render(request, 'events/mm/2019/mountain_madness2019.html', create_main_context(request, TAB))
+
+
 def mountain_madness2020(request):
     return render(request, 'events/mm/2020/mountain_madness2020.html', create_main_context(request, TAB))
 
