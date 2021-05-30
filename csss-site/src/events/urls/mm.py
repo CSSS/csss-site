4b1ec91db6a0ccd21f045ed49fc0d94b458591fd
Jace Manshadi <jmanshad@protonmail.com>
2021-05-30 01:04:27 -0700

    updating conventions used for the event urls and function names

--- csss-site/src/events/urls/mm.py
+++ csss-site/src/events/urls/mm.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from events.views import mm
 
 urlpatterns = [
-    url(r'^mountain_madness2019$', mm.mountain_madness2019, name='mountain_madness2019'),
-    url(r'^mountain_madness2020$', mm.mountain_madness2020, name='mountain_madness2020'),
-    url(r'^mountain_madness2021$', mm.mountain_madness2021, name='mountain_madness2021'),
+    url(r'^mm/2019$', mm.mm2019, name='mountain_madness2019'),
+    url(r'^mm/2020$', mm.mm2020, name='mountain_madness2020'),
+    url(r'^mm/2021$', mm.mm2021, name='mountain_madness2021'),
 ]
--- csss-site/src/events/views/mm.py
+++ csss-site/src/events/views/mm.py
@@ -5,13 +5,13 @@ from csss.views_helper import create_main_context
 TAB = 'events'
 
 
-def mountain_madness2019(request):
+def mm2019(request):
     return render(request, 'events/mm/2019/mountain_madness2019.html', create_main_context(request, TAB))
 
 
-def mountain_madness2020(request):
+def mm2020(request):
     return render(request, 'events/mm/2020/mountain_madness2020.html', create_main_context(request, TAB))
 
 
-def mountain_madness2021(request):
+def mm2021(request):
     return render(request, 'events/mm/2021/mountain_madness2021.html', create_main_context(request, TAB))
