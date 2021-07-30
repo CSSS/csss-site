79146089e1168b419cde959a061615df4912c834
Jace Manshadi <jmanshad@protonmail.com>
2021-07-29 21:46:14 -0700

    reverting name

--- csss-site/src/elections/views/create_election/webform/process_new_election_webform.py
+++ csss-site/src/elections/views/create_election/webform/process_new_election_webform.py
@@ -11,7 +11,7 @@ from elections.views.Constants import ELECTION_JSON_WEBFORM_KEY__TIME, ELECTION_
 from elections.views.create_context.webform.create_webform_context import \
     create_webform_election_context_from_user_inputted_election_dict
 from elections.views.save_election.save_new_election_from_jformat import save_new_election_from_jformat
-from elections.views.utils.transform_webform_to_json import transform_full_election_webform_to_json
+from elections.views.utils.transform_webform_to_json import transform_webform_to_json
 from elections.views.validators.validate_election_date import validate_webform_election_date_and_time
 from elections.views.validators.validate_election_type import validate_election_type
 from elections.views.validators.validate_link import validate_http_link
@@ -37,7 +37,7 @@ def process_new_inputted_webform_election(request, context):
      either redirect user back to the page where they inputted the election info or direct them to the newly created
       election page
     """
-    election_dict = transform_full_election_webform_to_json(parser.parse(request.POST.urlencode()))
+    election_dict = transform_webform_to_json(parser.parse(request.POST.urlencode()))
     if not verify_that_all_relevant_election_webform_keys_exist(election_dict):
         error_message = f"Did not find all of the following necessary keys in input: " \
                         f"{ELECTION_JSON_KEY__DATE}, {ELECTION_JSON_WEBFORM_KEY__TIME}, " \
