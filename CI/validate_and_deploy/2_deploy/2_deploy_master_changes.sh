60c703ea49916dca1734eb8cc364c6ea1a3f15fc
Jace Manshadi <jmanshad@protonmail.com>
2021-08-10 00:42:28 -0700

    removing duplicate entry

--- CI/validate_and_deploy/2_deploy/2_deploy_master_changes.sh
+++ CI/validate_and_deploy/2_deploy/2_deploy_master_changes.sh
@@ -22,7 +22,6 @@ function create_directory_for_website_logs {
   mkdir -p "${BASE_DIR}/website_logs/python_logs/update_officer_pics"
   mkdir -p "${BASE_DIR}/website_logs/python_logs/validate_resource_permissions"
   mkdir -p "${BASE_DIR}/website_logs/python_logs/create_fixtures"
-  mkdir -p "${BASE_DIR}/website_logs/python_logs/update_officer_pics"
   mkdir -p "${BASE_DIR}/website_logs/gunicorn_logs"
 }
 
