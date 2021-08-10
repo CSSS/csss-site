813917d30fb1b39041923af83e7fb54e1cadce9d
Jace Manshadi <jmanshad@protonmail.com>
2021-08-10 00:34:21 -0700

    Update 1_update_files_on_master_server.sh

--- CI/validate_and_deploy/2_deploy/1_update_files_on_master_server.sh
+++ CI/validate_and_deploy/2_deploy/1_update_files_on_master_server.sh
@@ -13,6 +13,7 @@ function remove_existing_files {
   ssh csss@"${HOST_ADDRESS}" "rm ${BASE_DIR}/site_envs/site_envs_gunicorn" || true
   ssh csss@"${HOST_ADDRESS}" "rm ${BASE_DIR}/site_envs/site_envs_create_fixtures" || true
   ssh csss@"${HOST_ADDRESS}" "rm ${BASE_DIR}/site_envs/site_envs_update_officer_pics" || true
+  ssh csss@"${HOST_ADDRESS}" "rm ${BASE_DIR}/site_envs/site_envs_validate_resource_permissions" || true
   ssh csss@"${HOST_ADDRESS}" "rm ${BASE_DIR}/set_env.sh" || true
   ssh csss@"${HOST_ADDRESS}" "rm ${BASE_DIR}/requirements.txt" || true
 
