36c042c97c1c255d98ee058623036aebe5b6ad48
Jace Manshadi <jmanshad@protonmail.com>
2021-08-11 22:59:47 -0700

    apparently I had forgotten to commit the csss setup command

---
+++ csss-site/src/csss/management/commands/setup_website.py
@@ -0,0 +1,30 @@
+import logging
+
+from django.core.management import BaseCommand
+
+from about.management.commands.update_officer_images import update_officer_images
+from announcements.management.commands.create_attachments import download_or_create_announcement_attachments
+
+logger = logging.getLogger('csss_site')
+
+
+class Command(BaseCommand):
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--download__attachments',
+            action='store_true',
+            default=False,
+            help="pull the latest email attachments from staging server"
+        )
+        parser.add_argument(
+            '--download__officer_images',
+            action='store_true',
+            default=False,
+            help="pull the latest exec-photos from the staging server"
+        )
+
+    def handle(self, *args, **options):
+        logger.info(options)
+        download_or_create_announcement_attachments(options['download__attachments'])
+        update_officer_images(options['download__officer_images'])
