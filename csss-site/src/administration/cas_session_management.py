25c326a4da4398836d68ef3ba9e43ada863997e1
Jace Manshadi <jmanshad@protonmail.com>
2021-08-14 01:25:46 -0700

    fixing flake8 issues

--- csss-site/src/administration/cas_session_management.py
+++ csss-site/src/administration/cas_session_management.py
@@ -24,58 +24,8 @@ def cas_login(request):
         response = requests.get(endpoint)
         try:
             sfu_cas_response = json.loads(json.dumps(xmltodict.parse(response.text)))
-
-            # only do this is the SFUID belong to user who was an officer in past 5 terms
             sfuid = sfu_cas_response['cas:serviceResponse']['cas:authenticationSuccess']['cas:user']
-            """
-            webmaster or DoA:
-            creating officer creation links -- webmaster or DoA
-            upload officer lists -- Webmaster or DoA
-            update position mappings -- webmaster or DoA
 
-
-            Sys Admin
-            update github mapping
-            github permission management
-
-
-            Webmaster or Sys Admin
-            accessing /admin
-
-            officer in past 5 years
-            current and past officer details
-            google drive permission management
-
-            Current Election Officer
-            CRU elections
-
-            how to determine whether to display login link or user's username -- if SFUID in cookies    
-
-
-
-            'authenticated': request.user.is_authenticated,
-            creating officer creation links
-            uploading officer lists
-            updating positions and github mapping
-            google drive and github permission management
-            how to determine whether to display login link or user's username
-            current and past officer details
-
-            'authenticated_officer': ('officer' in groups),
-            current and past officer details
-
-            ELECTION_MANAGEMENT_GROUP_NAME: (ELECTION_MANAGEMENT_GROUP_NAME in groups), //election_management && election_management_permission
-            create and modify elections
-
-            'staff': request.user.is_staff,
-            creating officer creation links
-            uploading officer lists
-            updating positions and github mapping
-            current and past officer details
-            google drive and github permission management
-            how to determine whether to display login link or user's username
-
-            """
             current_webmaster_or_doa_sfuid = get_list_of_officer_details_from_past_specified_terms(
                 relevant_previous_terms=0, position_names=["Webmaster", "Director of Archives"], filter_by_sfuid=True
             )
