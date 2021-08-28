bbeb8d5d281566c303d872741db0c40a220f408b
Jace Manshadi <jmanshad@protonmail.com>
2021-08-27 19:04:08 -0700

    fixing flake8 issues

--- csss-site/src/csss/views/request_validation.py
+++ csss-site/src/csss/views/request_validation.py
@@ -3,12 +3,6 @@ from csss.views.exceptions import InvalidPrivilege
 from csss.views.views import create_main_context
 
 
-def officer_request_allowed(request, groups=None):
-    if groups is None:
-        groups = list(request.user.groups.values_list('name', flat=True))
-    return 'officer' in groups
-
-
 def validate_officer_request(request, endpoint=None, html=None):
     groups = list(request.user.groups.values_list('name', flat=True))
     if html is None and endpoint is None:
@@ -18,7 +12,7 @@ def validate_officer_request(request, endpoint=None, html=None):
             "incorrect permission for officer access", context=create_main_context(request),
             html='csss/error.html'
         )
-    if officer_request_allowed(request, groups=groups):
+    if _officer_request_allowed(request, groups=groups):
         return
     if endpoint is not None:
         raise InvalidPrivilege(request, "You are not allowed to access this page", endpoint=endpoint)
@@ -35,7 +29,7 @@ def validate_request_to_manage_digital_resource_permissions(request, endpoint=No
             "incorrect permission for officer access", context=create_main_context(request),
             html='csss/error.html'
         )
-    if officer_request_allowed(request, groups=groups):
+    if _officer_request_allowed(request, groups=groups):
         return
     if endpoint is not None:
         raise InvalidPrivilege(request, "You are not allowed to access this page", endpoint=endpoint)
@@ -43,15 +37,15 @@ def validate_request_to_manage_digital_resource_permissions(request, endpoint=No
         raise InvalidPrivilege(request, "You are not allowed to access this page", html=html)
 
 
-def election_officer_request_allowed(request, groups=None):
+def _officer_request_allowed(request, groups=None):
     if groups is None:
         groups = list(request.user.groups.values_list('name', flat=True))
-    return ELECTION_MANAGEMENT_GROUP_NAME in groups
+    return 'officer' in groups
 
 
 def validate_request_to_delete_election(request, endpoint=None):
     groups = list(request.user.groups.values_list('name', flat=True))
-    if election_officer_request_allowed(request, groups=groups):
+    if _election_officer_request_allowed(request, groups=groups):
         return
     raise InvalidPrivilege(request, "You are not allowed to access this page", endpoint=endpoint)
 
@@ -65,9 +59,15 @@ def validate_request_to_manage_elections(request, endpoint=None, html=None):
             "incorrect permission for officer access", context=create_main_context(request),
             html='csss/error.html'
         )
-    if election_officer_request_allowed(request, groups=groups):
+    if _election_officer_request_allowed(request, groups=groups):
         return
     if endpoint is not None:
         raise InvalidPrivilege(request, "You are not allowed to access this page", endpoint=endpoint)
     if html is not None:
         raise InvalidPrivilege(request, "You are not allowed to access this page", html=html)
+
+
+def _election_officer_request_allowed(request, groups=None):
+    if groups is None:
+        groups = list(request.user.groups.values_list('name', flat=True))
+    return ELECTION_MANAGEMENT_GROUP_NAME in groups
