7053e91b271618c70bb18665d6137410b65b228a
Jace Manshadi <jmanshad@protonmail.com>
2021-09-07 22:44:12 -0700

    fixing how errors that are thrown without preparation are handled

--- csss-site/src/csss/views/error_handlers.py
+++ csss-site/src/csss/views/error_handlers.py
@@ -7,9 +7,6 @@ from csss.views.context_creation.create_main_context import create_main_context
 from csss.views.exceptions import InvalidPrivilege
 from csss.views.views import ERROR_MESSAGES_KEY
 
-ERROR_EXPERIENCED_KEY = 'error_experienced'
-
-
 class HandleBusinessExceptionMiddleware(MiddlewareMixin):
     def process_exception(self, request, exception):
         if isinstance(exception, InvalidPrivilege):
@@ -18,9 +15,9 @@ class HandleBusinessExceptionMiddleware(MiddlewareMixin):
             if exception.endpoint is not None and exception.error_messages is not None:
                 request.session[ERROR_MESSAGES_KEY] = exception.error_messages
                 return HttpResponseRedirect(exception.endpoint)
-        if isinstance(exception, DataError):
-            request.context[ERROR_MESSAGES_KEY] = [exception]
-            return render(request, request.html_page, request.context)
         context = create_main_context(request, 'index')
-        context[ERROR_EXPERIENCED_KEY] = [f"Encountered an unexpected exception of: {exception}"]
+        if isinstance(exception, DataError):
+            context[ERROR_MESSAGES_KEY] = [exception]
+        else:
+            context[ERROR_MESSAGES_KEY] = [f"Encountered an unexpected exception of: {exception}"]
         return render(request, 'csss/error.html', context)
