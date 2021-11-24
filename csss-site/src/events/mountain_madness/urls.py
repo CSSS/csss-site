60c6203153e5abb7569af9148656d69d0bdb58c3
Mitchell Gale <mitchyg99@gmail.com>
2021-11-23 17:21:04 -0800

    Added page for Mountain Madness 2021 submissions (#420)
    * Added page for Mountain Madness 2021 submissions
    
    * Fixed validation issues
    
    Co-authored-by: mgale <mgale@sfu.ca>
    Co-authored-by: Jace Manshadi <jmanshad@protonmail.com>

--- csss-site/src/events/mountain_madness/urls.py
+++ csss-site/src/events/mountain_madness/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from events.mountain_madness import views
 
 urlpatterns = [
+    url(r'^2021/submissions$', views.mm2021_submissions, name='mountain_madness2021_submissions'),
     url(r'^2021$', views.mm2021, name='mountain_madness2021'),
     url(r'^2020$', views.mm2020, name='mountain_madness2020'),
     url(r'^2019$', views.mm2019, name='mountain_madness2019'),
