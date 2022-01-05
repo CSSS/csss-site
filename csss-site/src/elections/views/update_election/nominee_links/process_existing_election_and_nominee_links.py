91059aa4396784ee4fd61d0848f7aea2f3340f38
Jace Manshadi <jmanshad@protonmail.com>
2022-01-04 19:06:20 -0800

    fixing the imports

--- csss-site/src/elections/views/update_election/nominee_links/process_existing_election_and_nominee_links.py
+++ csss-site/src/elections/views/update_election/nominee_links/process_existing_election_and_nominee_links.py
@@ -9,7 +9,8 @@ from elections.views.Constants import SAVED_NOMINEE_LINKS, \
     ENDPOINT_MODIFY_VIA_NOMINEE_LINKS
 from elections.views.ElectionModelConstants import ELECTION_JSON_KEY__DATE, ELECTION_JSON_WEBFORM_KEY__TIME, \
     ELECTION_JSON_KEY__ELECTION_TYPE, ELECTION_JSON_KEY__WEBSURVEY
-from elections.views.create_context.nominee_links.create_or_update_election.create_context_for_update_election_nominee_links_html import \
+from elections.views.create_context.nominee_links.create_or_update_election.\
+    create_context_for_update_election_nominee_links_html import \
     create_context_for_update_election_nominee_links_html
 from elections.views.save_election.save_existing_election_obj_jformat import update_existing_election_obj_from_jformat
 from elections.views.save_election.save_new_nominee_links_from_jformat import save_new_nominee_links_from_jformat
