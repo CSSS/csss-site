7573030d78f54e0cbe63534f28536e89b2fdab55
Jace Manshadi <jmanshad@protonmail.com>
2022-03-07 19:47:03 -0800

    print the stracktrace to the logs

--- csss-site/src/csss/views/error_handlers.py
+++ csss-site/src/csss/views/error_handlers.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.utils.deprecation import MiddlewareMixin
+import traceback
+import logging
 
 from csss.views.context_creation.create_main_context import create_main_context
 from csss.views.exceptions import InvalidPrivilege, NoAuthenticationMethod, CASAuthenticationMethod
 from csss.views.views import ERROR_MESSAGES_KEY
 
+logger = logging.getLogger('csss_site')
 
 class HandleBusinessExceptionMiddleware(MiddlewareMixin):
     def process_exception(self, request, exception):
@@ -15,5 +18,6 @@ class HandleBusinessExceptionMiddleware(MiddlewareMixin):
         if isinstance(exception, CASAuthenticationMethod):
             return exception.render
         context = create_main_context(request, 'index')
+        logger.info(traceback.format_exc())
         context[ERROR_MESSAGES_KEY] = [f"Encountered an unexpected exception of: {exception}"]
         return render(request, 'csss/error_htmls/unknown_error.html', context)
