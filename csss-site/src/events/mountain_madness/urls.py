91766afecf4b055eda2ea76b0d29b40a2ce49b21
Mitchell Gale <mitchyg99@gmail.com>
2022-04-22 20:52:17 -0700

    Added page for Mountain Madness 2022 (#440)
    * Adding mountain Madness website 2022
    
    Co-authored-by: mgale <mgale@sfu.ca>

--- csss-site/src/events/mountain_madness/urls.py
+++ csss-site/src/events/mountain_madness/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from events.mountain_madness import views
 
 urlpatterns = [
+    url(r'^2022$', views.mm2022, name='mountain_madness2022'),
     url(r'^2021/submissions$', views.mm2021_submissions, name='mountain_madness2021_submissions'),
     url(r'^2021$', views.mm2021, name='mountain_madness2021'),
     url(r'^2020$', views.mm2020, name='mountain_madness2020'),
