51158415b8a3f9a6fafacd91c69b0ae3406921ec
Jace Manshadi <jmanshad@protonmail.com>
2022-08-13 03:18:54 -0700

    just to make it easier to find users that don't have their discord information uploaded yet

--- csss-site/src/about/admin.py
+++ csss-site/src/about/admin.py
@@ -13,7 +13,7 @@ admin.site.register(Term, TermAdmin)
 
 class OfficerAdmin(admin.ModelAdmin):
     list_display = (
-        'full_name', 'elected_term', 'position_index', 'position_name', 'start_date',
+        'full_name', 'elected_term', 'discord_id', 'position_index', 'position_name', 'start_date',
         'sfu_computing_id', 'sfu_email_alias', 'phone_number', 'github_username', 'gmail',
         'course1', 'course2', 'language1', 'language2'
     )
