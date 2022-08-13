d149ed78009d27e8e2a4c305020ba3e28c25a2f6
Jace Manshadi <jmanshad@protonmail.com>
2022-08-13 02:32:29 -0700

    small fixes (#456)
    * small fixes

--- csss-site/src/about/management/commands/update_discord_details.py
+++ csss-site/src/about/management/commands/update_discord_details.py
@@ -1,4 +1,5 @@
 import logging
+from time import sleep
 
 from django.core.management import BaseCommand
 
@@ -15,10 +16,18 @@ class Command(BaseCommand):
         officers = Officer.objects.all().exclude(discord_id="NA")
         officers_discord_ids = list(set(list(officers.values_list('discord_id', flat=True))))
         discord_info_maps = {}
+        max_retries = 5
         for officers_discord_id in officers_discord_ids:
             success, error_message, discord_username, discord_nickname = get_discord_username_and_nickname(
                 officers_discord_id
             )
+            retries = 0
+            while not success and retries < max_retries:
+                sleep(10)
+                success, error_message, discord_username, discord_nickname = get_discord_username_and_nickname(
+                    officers_discord_id
+                )
+                retries += 1
             officer = officers.filter(discord_id=officers_discord_id).first()
             if success:
                 logger.info(
@@ -33,12 +42,12 @@ class Command(BaseCommand):
             else:
                 logger.info(
                     f"[about/update_discord_details.py()] unable to get the discord username and nickname for "
-                    f"{officer.full_name}"
+                    f"{officer.full_name} due to error {error_message}"
                 )
-        officers_to_change = officers.filter(discord_id__in=discord_info_maps.keys())
+        officers_to_change = officers.filter(sfu_computing_id__in=list(discord_info_maps.keys()))
 
         for officer in officers_to_change:
             officer.discord_id = discord_info_maps[officer.sfu_computing_id]['officers_discord_id']
-            officer.discord_username = discord_info_maps[officer.discord_id]['discord_username']
-            officer.discord_nickname = discord_info_maps[officer.discord_id]['discord_nickname']
+            officer.discord_username = discord_info_maps[officer.sfu_computing_id]['discord_username']
+            officer.discord_nickname = discord_info_maps[officer.sfu_computing_id]['discord_nickname']
         Officer.objects.bulk_update(officers_to_change, ['discord_id', 'discord_username', 'discord_nickname'])
