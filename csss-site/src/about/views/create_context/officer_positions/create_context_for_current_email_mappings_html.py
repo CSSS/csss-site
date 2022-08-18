5ab1807084e8ec3695c7e162a771aae463382b4a
Jace Manshadi <jmanshad@protonmail.com>
2022-08-17 22:20:05 -0700

    creating a entry for the current executive officer of each position

--- csss-site/src/about/views/create_context/officer_positions/create_context_for_current_email_mappings_html.py
+++ csss-site/src/about/views/create_context/officer_positions/create_context_for_current_email_mappings_html.py
@@ -11,8 +11,8 @@ def create_context_for_current_email_mappings_html(context, officer_emaillist_an
             term_number__in=get_relevant_terms()
         )
     ).order_by('-start_date')
-    current_and_past_minus_1_officers = [CURRENT_EXEC_EMAIL_LIST]
-    current_officers = []
+    current_and_past_minus_1_executive_officers = [CURRENT_EXEC_EMAIL_LIST]
+    current_executive_officers = []
     for officer_emaillist_and_position_mapping in officer_emaillist_and_position_mappings:
         if officer_emaillist_and_position_mapping.email != "NA":
             officers_to_show = officers.filter(
@@ -20,19 +20,23 @@ def create_context_for_current_email_mappings_html(context, officer_emaillist_an
             ).order_by('-start_date')[:2]
             officers_to_show = list(set([officer_to_show.sfu_computing_id for officer_to_show in officers_to_show]))
             if officer_emaillist_and_position_mapping.executive_officer:
-                current_and_past_minus_1_officers.extend(officers_to_show[1:3])
-                current_officers.extend(officers_to_show[:1])
+                current_and_past_minus_1_executive_officers.extend(officers_to_show[1:3])
+                current_executive_officers.extend(officers_to_show[:1])
             context[EMAIL_LIST_MAPPINGS].append({
-                SFU_EMAIL_LIST: officer_emaillist_and_position_mapping.email,
+                SFU_EMAIL_LIST: officer_emaillist_and_position_mapping.email.replace("-current", ""),
                 SFU_COMPUTING_IDS: officers_to_show
             })
-    current_officers = list(set(current_officers))
-    current_and_past_minus_1_officers = list(set(current_and_past_minus_1_officers))
+            context[EMAIL_LIST_MAPPINGS].append({
+                SFU_EMAIL_LIST: officer_emaillist_and_position_mapping.email,
+                SFU_COMPUTING_IDS: officers_to_show[:1]
+            })
+    current_executive_officers = list(set(current_executive_officers))
+    current_and_past_minus_1_executive_officers = list(set(current_and_past_minus_1_executive_officers))
     context[EMAIL_LIST_MAPPINGS].append({
         SFU_EMAIL_LIST: CURRENT_EXEC_EMAIL_LIST,
-        SFU_COMPUTING_IDS: current_officers
+        SFU_COMPUTING_IDS: current_executive_officers
     })
     context[EMAIL_LIST_MAPPINGS].append({
         SFU_EMAIL_LIST: CURRENT_AND_PAST_MINUS_1_EXEC_EMAIL_LIST,
-        SFU_COMPUTING_IDS: current_and_past_minus_1_officers
+        SFU_COMPUTING_IDS: current_and_past_minus_1_executive_officers
     })
