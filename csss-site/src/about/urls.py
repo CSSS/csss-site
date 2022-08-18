dd642fb88a64d8a2bde1258f795146c518277ee4
Jace Manshadi <jmanshad@protonmail.com>
2022-08-17 19:27:39 -0700

    removing outdated code

--- csss-site/src/about/urls.py
+++ csss-site/src/about/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import url
 
 from .views import officer_management, import_export_officer_lists
 from .views.Constants import ENTER_NEW_OFFICER_INFO_URL, CURRENT_EMAIL_MAPPINGS_URL
-from .views.generate_officer_creation_links import officer_creation_link_management
 from .views.github_position_mapping.github_mapping import github_mapping
 from .views.github_position_mapping.save_new_github_officer_team_mapping import save_new_github_officer_team_mapping
 from .views.github_position_mapping.update_saved_github_mappings import update_saved_github_mappings
@@ -19,10 +18,6 @@ urlpatterns = [
     url(r'^specify_new_officers/delete$', delete_new_officers, name='Specify Positions for New Officers'),
     url(fr'^{ENTER_NEW_OFFICER_INFO_URL}$', enter_new_officer_info, name='Allow officer to enter their info'),
     url(fr'^{CURRENT_EMAIL_MAPPINGS_URL}$', current_email_mapping, name="Get the current mapping workflow"),
-    url(
-        r'^show_create_link_page$',
-        officer_creation_link_management.show_create_link_page,
-        name='Show Page For Creating Links for Officer Information Input'),
     url(
         r'^officer_position$', officer_positions,
         name="Officer Position Mapping"
@@ -59,23 +54,6 @@ urlpatterns = [
     url(r'^list_of_past_officers$', officer_management.list_of_past_officers, name='CSSS List of Past Officers'),
     url(r'^who_we_are$', officer_management.who_we_are, name='Who We Are'),
 
-
-    url(
-        r'^show_page_with_creation_links$',
-        officer_creation_link_management.show_page_with_creation_links,
-        name="Create and display links that are used by officer to input their information"),
-    url(
-        r'^allow_officer_to_choose_name$',
-        officer_creation_link_management.allow_officer_to_choose_name,
-        name='Add an Officer'),
-    url(
-        r'^display_page_for_officer_to_input_info$',
-        officer_creation_link_management.display_page_for_officers_to_input_their_info,
-        name='Add an Officer'),
-    url(
-        r'^input_officer_info$',
-        officer_creation_link_management.process_information_entered_by_officer,
-        name='Add an Officer'),
 
     url(
         r'^show_page_for_inputting_officer_info$',
