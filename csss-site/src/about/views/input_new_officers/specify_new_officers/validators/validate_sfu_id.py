74481dc515b202ff48b210635a928b3942c1f426
Jace Manshadi <jmanshad@protonmail.com>
2022-09-28 17:36:14 -0700

    sometimes the connection get reset by sfu's server, so will set the website to wait a second before trying again

--- csss-site/src/about/views/input_new_officers/specify_new_officers/validators/validate_sfu_id.py
+++ csss-site/src/about/views/input_new_officers/specify_new_officers/validators/validate_sfu_id.py
@@ -1,4 +1,5 @@
 import json
+from time import sleep
 
 import requests
 from django.conf import settings
@@ -17,10 +18,21 @@ def validate_sfu_id(sfu_computing_id):
     """
     if settings.SFU_ENDPOINT_TOKEN is None:
         return True, None
-    resp = requests.get(
-        f"https://rest.its.sfu.ca/cgi-bin/WebObjects/AOBRestServer.woa/rest/amaint/namespace.json?"
-        f"id={sfu_computing_id}&art={settings.SFU_ENDPOINT_TOKEN}"
-    )
+    connection_reset = False
+    first_try = True
+    while connection_reset or first_try:
+        if connection_reset:
+            sleep(1)
+        connection_reset = False
+        first_try = False
+        try:
+            resp = requests.get(
+                f"https://rest.its.sfu.ca/cgi-bin/WebObjects/AOBRestServer.woa/rest/amaint/namespace.json?"
+                f"id={sfu_computing_id}&art={settings.SFU_ENDPOINT_TOKEN}"
+            )
+        except Exception as e:
+            if len(e.args) > 0 and len(e.args[0].args) > 1 and len(e.args[0].args[1].args) > 1:
+                connection_reset = e.args[0].args[1].args[1] == 'Connection reset by peer'
     if resp.status_code != 200:
         return False, f"Encountered error message of '{resp.reason}'"
     if not (json.loads(resp.text)['type'] == "username" and json.loads(resp.text)['username'] == sfu_computing_id):
