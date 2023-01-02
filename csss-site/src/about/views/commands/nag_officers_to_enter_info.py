d74aa650781356ca5006a4c727bbb07bccbc194f
Jace Manshadi <jmanshad@protonmail.com>
2023-01-02 01:32:39 -0800

    refactoring how the nagging works since the officer is incentivized enough to enter their info by the fact that they have lost access to their resouces, just making sure that the DoA knows that there is still something left to do

--- csss-site/src/about/views/commands/nag_officers_to_enter_info.py
+++ csss-site/src/about/views/commands/nag_officers_to_enter_info.py
@@ -2,9 +2,6 @@ import logging
 from time import sleep
 
 from about.models import UnProcessedOfficer, Officer
-from about.views.input_new_officers.specify_new_officers.notifications. \
-    send_notification_asking_officer_to_fill_in_form import \
-    send_notification_asking_officer_to_fill_in_form
 from csss.views.send_discord_dm import send_discord_dm
 
 logger = logging.getLogger('csss_site')
@@ -12,23 +9,27 @@ logger = logging.getLogger('csss_site')
 
 def run_job():
     unprocessed_officers = UnProcessedOfficer.objects.all()
-    officers = Officer.objects.all()
+    current_director_of_archives = Officer.objects.all().filter(
+        position_name='Director of Archives'
+    ).order_by('-start_date').first()
+    current_systems_admin = Officer.objects.all().filter(
+        position_name='Systems Administrator'
+    ).order_by('-start_date').first()
     for unprocessed_officer in unprocessed_officers:
         sleep(1)
-        success, error_message = send_notification_asking_officer_to_fill_in_form(
-            unprocessed_officer.discord_id,
-            unprocessed_officer.full_name,
-            officers.filter(sfu_computing_id=unprocessed_officer.sfu_computing_id).first() is None
-        )
         unprocessed_officer.number_of_nags += 1
         unprocessed_officer.save()
         if (unprocessed_officer.number_of_nags % 3) == 0:
             send_discord_dm(
-                '288148680479997963', "unfilled in officer data",
+                current_systems_admin.discord_id, "unfilled in officer data",
+                f"{unprocessed_officer.full_name} still has not filled in their data..."
+            )
+        if current_director_of_archives is not None:
+            send_discord_dm(
+                current_director_of_archives.discord_id, "unfilled in officer data",
                 f"{unprocessed_officer.full_name} still has not filled in their data..."
             )
         logger.info(
-            f"[about/nag_officers_to_enter_info.py()] {'nagged ' if success else 'was not able to nag '}"
-            f"{unprocessed_officer.full_name} to fill in their info"
-            f"{'' if success else f' due to error {error_message}'}."
+            f"[about/nag_officers_to_enter_info.py()] alerted the Sys Admin and DoA that "
+            f"{unprocessed_officer.full_name} has not filled in their info"
         )
