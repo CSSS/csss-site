f4cceb514d777b2fc260c214bed98752ad57d91e
Jace Manshadi <jmanshad@protonmail.com>
2023-01-01 16:24:36 -0800

    refactoring command to a central script with the "run_job" function to prepare it for the cron job logic (#508)

--- csss-site/src/about/management/commands/update_discord_details.py
+++ csss-site/src/about/management/commands/update_discord_details.py
@@ -1,56 +1,10 @@
-import logging
-from time import sleep
-
 from django.core.management import BaseCommand
 
-from about.models import Officer
-from about.views.utils.discord.get_discord_username_and_nickname import get_discord_username_and_nickname
-
-logger = logging.getLogger('csss_site')
+from about.views.commands.update_discord_details import run_job
 
 
 class Command(BaseCommand):
     help = "get the latest discord name and nicknames for the officers"
 
     def handle(self, *args, **options):
-        all_officers = Officer.objects.all()
-        officers = all_officers.exclude(discord_id="NA")
-        officers_discord_ids = list(set(list(officers.values_list('discord_id', flat=True))))
-        discord_info_maps = {}
-        max_retries = 5
-        for officers_discord_id in officers_discord_ids:
-            sleep(1)
-            success, error_message, discord_username, discord_nickname = get_discord_username_and_nickname(
-                officers_discord_id
-            )
-            retries = 0
-            while not success and retries < max_retries:
-                sleep(10)
-                success, error_message, discord_username, discord_nickname = get_discord_username_and_nickname(
-                    officers_discord_id
-                )
-                retries += 1
-            officer = officers.filter(discord_id=officers_discord_id).first()
-            if success:
-                logger.info(
-                    f"[about/update_discord_details.py()] the nickname or username for {officer.full_name} "
-                    f"was update since last time"
-                )
-                discord_info_maps[officer.sfu_computing_id] = {
-                    'officers_discord_id': officers_discord_id,
-                    'discord_username': discord_username,
-                    'discord_nickname': discord_nickname
-                }
-            else:
-                logger.info(
-                    f"[about/update_discord_details.py()] unable to get the discord username and nickname for "
-                    f"{officer.full_name} due to error {error_message}"
-                )
-        officers_to_change = all_officers.filter(sfu_computing_id__in=list(discord_info_maps.keys()))
-
-        for officer in officers_to_change:
-            officer.discord_id = discord_info_maps[officer.sfu_computing_id]['officers_discord_id']
-            officer.discord_username = discord_info_maps[officer.sfu_computing_id]['discord_username']
-            nickname = discord_info_maps[officer.sfu_computing_id]['discord_nickname']
-            officer.discord_nickname = nickname if nickname is not None else "NA"
-        Officer.objects.bulk_update(officers_to_change, ['discord_id', 'discord_username', 'discord_nickname'])
+        run_job()
