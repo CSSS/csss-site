8618042d07909f9ea5c4339435d79d9009021451
Jace Manshadi <jmanshad@protonmail.com>
2023-01-27 15:44:55 -0800

    small fixes to error checking

--- csss-site/src/csss/CSSSLoggerHandlers.py
+++ csss-site/src/csss/CSSSLoggerHandlers.py
@@ -29,7 +29,7 @@ class CSSSErrorHandler(logging.StreamHandler):
         else:
             # django-commands come here
             filename = self.file_name
-            message = record.exc_test if record.exc_text is not None else record.message
+            message = record.exc_text if record.exc_text is not None else record.message
             record_type = 'other_record'
         if len(CSSSError.objects.all().filter(message=message)) == 0:
             CSSSError(filename=filename, message=message, request=request, endpoint=endpoint, type=record_type).save()
