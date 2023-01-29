e2dcef414a1144a2623035ab92ab478773cf1f17
liversticks <oliverxie19@gmail.com>
2023-01-28 20:30:49 -0800

    MM 2023: migrate website (some details preliminary

--- csss-site/src/events/mountain_madness/urls.py
+++ csss-site/src/events/mountain_madness/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from events.mountain_madness import views
 
 urlpatterns = [
+    url(r'^2023$', views.mm2023, name='mountain_madness2023'),
     url(r'^2022$', views.mm2022, name='mountain_madness2022'),
     url(r'^2021/submissions$', views.mm2021_submissions, name='mountain_madness2021_submissions'),
     url(r'^2021$', views.mm2021, name='mountain_madness2021'),
