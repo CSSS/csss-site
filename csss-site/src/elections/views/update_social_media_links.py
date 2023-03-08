060e78a34c750349ae9709f7df87c5d87f19460d
Jace Manshadi <jmanshad@protonmail.com>
2023-03-08 14:29:56 -0800

    adding code to get any SFU IDs from the officer object if possible

--- csss-site/src/elections/views/update_social_media_links.py
+++ csss-site/src/elections/views/update_social_media_links.py
@@ -1,31 +1,16 @@
 from django.http import HttpResponseRedirect
 
+from about.models import Officer
 from elections.models import Nominee
 
 
 def update_social_media_links(request):
-    nominees = Nominee.objects.all().order_by('full_name')
-    no_entry_options = ["NA", "NONE"]
-    for nominee in nominees:
-        if nominee.discord_username in no_entry_options:
-            nominee.discord_username = None
-        if nominee.discord in no_entry_options:
-            nominee.discord = None
-        else:
-            if nominee.discord_username is None:
-                nominee.discord_username = nominee.discord
-        if nominee.discord_nickname in no_entry_options:
-            nominee.discord_nickname = None
-        if nominee.instagram in no_entry_options:
-            nominee.instagram = None
-        if nominee.discord_id in no_entry_options:
-            nominee.discord_id = None
-        if nominee.email in no_entry_options:
-            nominee.email = None
-        if nominee.facebook in no_entry_options:
-            nominee.facebook = None
-        if nominee.linkedin in no_entry_options:
-            nominee.linkedin = None
-        nominee.save()
-        print(nominee)
+    officers_dict = {
+        officer.full_name: officer.sfu_computing_id
+        for officer in Officer.objects.all().exclude(sfu_computing_id="NA").exclude(sfu_computing_id="")
+    }
+    for nominee in Nominee.objects.all():
+        if nominee.full_name in officers_dict:
+            nominee.sfuid = officers_dict[nominee.full_name]
+            nominee.save()
     return HttpResponseRedirect("/elections/")
