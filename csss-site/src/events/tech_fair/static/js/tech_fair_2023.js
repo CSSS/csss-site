665d3e921d0785da52acc2635f749b08d3d80fda
Gabe WSL Debian <24978329+EarthenSky@users.noreply.github.com>
2023-06-03 16:46:25 -0700

    initial styling of site

---
+++ csss-site/src/events/tech_fair/static/js/tech_fair_2023.js
@@ -0,0 +1,6 @@
+
+function bodyLoaded() {
+    document.getElementById("info-banner").hidden = false;
+    document.getElementById("info-banner").style.height = document.getElementById("info-text").clientHeight + 24 + "px";
+    console.log("yep");
+}
