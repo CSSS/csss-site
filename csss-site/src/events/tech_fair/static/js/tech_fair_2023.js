e31dafe1bcb5d4fa762a9a708165a39591c529f9
Gabe WSL Debian <24978329+EarthenSky@users.noreply.github.com>
2023-06-06 02:21:10 -0700

    update top bar & title of website

--- csss-site/src/events/tech_fair/static/js/tech_fair_2023.js
+++ csss-site/src/events/tech_fair/static/js/tech_fair_2023.js
@@ -1,6 +1,37 @@
+const laptop = document.getElementById("laptop");
+const laptopDiv = document.getElementById("laptop-div");
+var mouseIn = false;
 
-function bodyLoaded() {
+function onload() {
+    updateBanner();
+
+    laptopDiv.addEventListener("mouseenter", (event) => {
+        //event.target.getSVGDocument().getElementById("main").setAttribute("fill", "#97f");
+        mouseIn = true;
+    }, false);
+    laptopDiv.addEventListener("mouseleave", (event) => {
+        //event.target.getSVGDocument().getElementById("main").setAttribute("fill", "black");
+        mouseIn = false;
+    }, false);
+
+    window.requestAnimationFrame(update);
+}
+
+function update(timeStamp) {
+    let amount = mouseIn ? -8 : 18;
+    laptop.style.marginLeft = parseInt(laptop.style.marginLeft) + amount + "px";
+    if (parseInt(laptop.style.marginLeft) <= 0) {
+        laptop.style.marginLeft = "0px";
+    } else if(parseInt(laptop.style.marginLeft) >= 240) {
+        laptop.style.marginLeft = "240px";
+    }
+    window.requestAnimationFrame(update);
+}
+
+function updateBanner() {
     document.getElementById("info-banner").hidden = false;
     document.getElementById("info-banner").style.height = document.getElementById("info-text").clientHeight + 24 + "px";
     console.log("yep");
 }
+
+
