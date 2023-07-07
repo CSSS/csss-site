d7eabe4cd64417febf46c8c7f3bb9c47a5cd2a79
Jace Manshadi <jmanshad@protonmail.com>
2023-07-07 07:57:41 -0700

    forgot to update the cron scripts to reflect the new location and naming standard of the env files

--- csss-site/src/resource_management/cron_scripts/prod_validate_google_workspace_shared_team_drive_for_general_documents.sh
+++ csss-site/src/resource_management/cron_scripts/prod_validate_google_workspace_shared_team_drive_for_general_documents.sh
@@ -2,6 +2,6 @@
 
 cd /home/csss;
 . envCSSS/bin/activate;
-. ./set_env.sh site_envs/site_envs_gunicorn;
+. ./set_env.sh csss_site_envs/csss_site_gunicorn.env;
 cd csss-site;
 python manage.py validate_google_workspace_shared_team_drive_for_general_documents;
