410cc10cdf00b6ec53101e7243519e9d326bc7f0
Jace Manshadi <jmanshad@protonmail.com>
2023-07-22 12:24:47 -0700

    adding back-end validation for google drive link and finally adding the event specifier to the upload object

--- csss-site/src/resource_management/views/process_uploaded_media.py
+++ csss-site/src/resource_management/views/process_uploaded_media.py
@@ -1,4 +1,5 @@
 import datetime
+import re
 
 from django.http import HttpResponseRedirect
 
@@ -6,7 +7,8 @@ from csss.settings import URL_ROOT
 from csss.views.send_email import send_email
 from resource_management.models import Upload, MediaUpload
 from resource_management.views.Constants import MEDIA_UPLOADS__HTML_NAME, MEDIA_UPLOADS_NOTE__HTML_NAME, \
-    MEDIA_UPLOADS_EVENT_TYPE__HTML_NAME, MEDIA_UPLOADS_EVENT_DATE__HTML_NAME
+    MEDIA_UPLOADS_EVENT_TYPE__HTML_NAME, MEDIA_UPLOADS_EVENT_DATE__HTML_NAME, \
+    MEDIA_UPLOADS_EVENT_TYPE_SPECIFIER__HTML_NAME
 
 
 def process_uploaded_media(request):
@@ -16,15 +18,23 @@ def process_uploaded_media(request):
     media_uploaded = (
         request.FILES.get(MEDIA_UPLOADS__HTML_NAME, None) is not None  # if the user uploaded some media
         or
-        request.POST[MEDIA_UPLOADS_NOTE__HTML_NAME] != ""
-        # if the user just added a note that the front-end validation ensured is a Google Drive link
+        (
+            request.POST[MEDIA_UPLOADS_NOTE__HTML_NAME] != "" and
+            re.match(r"https://drive.google.com/drive/folders/[a-zA-Z0-9_]",
+                     request.POST[MEDIA_UPLOADS_NOTE__HTML_NAME])
+         )
+        # if the user just added a note that is a google drive link
     )
     if media_uploaded:
+        upload_has_specifier = request.POST[MEDIA_UPLOADS_EVENT_TYPE__HTML_NAME] == 'Other'
+        event_type_specifier = request.POST[MEDIA_UPLOADS_EVENT_TYPE_SPECIFIER__HTML_NAME] \
+            if upload_has_specifier else None
         upload = Upload(
             upload_date=datetime.datetime.now(),
             event_type=request.POST[MEDIA_UPLOADS_EVENT_TYPE__HTML_NAME],
             event_date=request.POST[MEDIA_UPLOADS_EVENT_DATE__HTML_NAME],
-            relevant_note=request.POST[MEDIA_UPLOADS_NOTE__HTML_NAME]
+            relevant_note=request.POST[MEDIA_UPLOADS_NOTE__HTML_NAME],
+            event_type_specifier=event_type_specifier
         )
         upload.save()
         if request.FILES.get(MEDIA_UPLOADS__HTML_NAME, None) is not None:
