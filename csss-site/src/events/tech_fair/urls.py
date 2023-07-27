cc53504e023e386c4dfb05c7b97853f22d66ebbe
Gabe WSL Debian <24978329+EarthenSky@users.noreply.github.com>
2023-07-26 17:33:50 -0700

    finalize tech fair 2023 website

--- csss-site/src/events/tech_fair/urls.py
+++ csss-site/src/events/tech_fair/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^2022$', views.tech_fair2022, name='tech_fair2022'),
     url(r'^2022/main$', views.tech_fair2022_main, name='tech_fair2022_main'),
     url(r'^2023$', views.tech_fair_2023, name='tech_fair_2023'),
+    url(r'^2023/company_package$', views.tech_fair_2023_company_package, name='tech_fair_2023_pkg'),
 ]
