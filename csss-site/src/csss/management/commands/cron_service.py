312cbebfa0adc4160404bbde90cebde3a6ba3403
Jace Manshadi <jaymanshad@proton.me>
2023-09-21 13:58:40 -0700

    updating code so it alerts the sys-admin that it had to create a new cron job

--- csss-site/src/csss/management/commands/cron_service.py
+++ csss-site/src/csss/management/commands/cron_service.py
@@ -2,7 +2,6 @@ import datetime
 import importlib
 from time import sleep
 
-from apscheduler.jobstores.base import JobLookupError
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.cron import CronTrigger
 from django.core.management import BaseCommand
@@ -45,31 +44,39 @@ class Command(BaseCommand):
             date = datetime.datetime.now(date_timezone)
             for updated_cron_job in updated_cron_jobs:
                 job = scheduler.get_job(updated_cron_job.job_id)
-                if updated_cron_job.schedule == "":
-                    logger.info(
-                        f"[Cron_Service_Command handle()] removing cron job {updated_cron_job.job_name} since the "
-                        f"schedule is now empty "
-                    )
-                    scheduler.remove_job(job_id=job.id)
-                    logger.info(f"[Cron_Service_Command handle()] cron job {updated_cron_job.job_name} removed")
-                else:
-                    logger.info(
-                        f"[Cron_Service_Command handle()] updating cron job {updated_cron_job.job_name} "
-                        f"with schedule of {job._scheduler} from {job.trigger} to "
-                        f"{updated_cron_job.schedule}"
-                    )
+                if job is None:
                     script_path = CRON_JOB_MAPPING[updated_cron_job.job_name][CRON_JOB_MAPPING_PATH_KEY]
-                    try:
+                    job = scheduler.add_job(
+                        importlib.import_module(f'{script_path}{updated_cron_job.job_name}').run_job,
+                        trigger=CronTrigger.from_crontab(updated_cron_job.schedule, timezone=TIME_ZONE)
+                    )
+                    previous_job_id = updated_cron_job.job_id
+                    updated_cron_job.job_id = job.id
+                    updated_cron_job.save()
+                    logger.error(
+                        "[Cron_Service_Command handle()] unable to find a cron job id of "
+                        f"{previous_job_id} for {updated_cron_job.job_name}."
+                        f"A new cron job was created as a result with id of {updated_cron_job.job_id}"
+                    )
+                else:
+                    if updated_cron_job.schedule == "":
+                        logger.info(
+                            f"[Cron_Service_Command handle()] removing cron job {updated_cron_job.job_name}"
+                            f" since the schedule is now empty "
+                        )
+                        scheduler.remove_job(job_id=job.id)
+                        logger.info(f"[Cron_Service_Command handle()] cron job {updated_cron_job.job_name} removed")
+                    else:
+                        logger.info(
+                            f"[Cron_Service_Command handle()] updating cron job {updated_cron_job.job_name} "
+                            f"with schedule of {job.trigger} to {updated_cron_job.schedule}"
+                        )
+                        script_path = CRON_JOB_MAPPING[updated_cron_job.job_name][CRON_JOB_MAPPING_PATH_KEY]
                         scheduler.reschedule_job(
-                            updated_cron_job.job_id,
+                            job.id,
                             importlib.import_module(f'{script_path}{updated_cron_job.job_name}').run_job,
-                            trigger=CronTrigger.from_crontab(updated_cron_job.schedule)
+                            trigger=CronTrigger.from_crontab(updated_cron_job.schedule, timezone=TIME_ZONE)
                         )
-                    except JobLookupError:
-                        logger.error(
-                            f"[Cron_Service_Command handle()] unable to find a cron job of {updated_cron_job.job_id}."
-                            f"A restart of the cron-service is recommended."
-                        )
-                    logger.info(f"[Cron_Service_Command handle()] updated cron job {updated_cron_job.job_name} ")
+                        logger.info(f"[Cron_Service_Command handle()] updated cron job {updated_cron_job.job_name} ")
             logger.info("[Cron_Service_Command handle()] going to sleep for an hour... ")
             sleep(seconds_in_an_hour)
