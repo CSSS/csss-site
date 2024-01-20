e2b997d9e6906107ebc1de37e739b836d91bd194
Jace Manshadi <jaymanshad@proton.me>
2024-01-20 11:09:57 -0800

    fixing test dockerfile for linter

--- CI/validate_and_deploy/1_validate/run_local_formatting_test.sh
+++ CI/validate_and_deploy/1_validate/run_local_formatting_test.sh
@@ -6,11 +6,9 @@
 docker stop csss_website_test || true
 docker rm csss_website_test || true
 docker image rm csss_website_test || true
-pushd ../../
 docker build -t csss_website_test -f CI/validate_and_deploy/1_validate/Dockerfile.test \
  --build-arg CONTAINER_HOME_DIR=/usr/src/app --build-arg UNIT_TEST_RESULTS=/usr/src/app/tests \
   --build-arg TEST_RESULT_FILE_NAME=all-unit-tests.xml .
-popd
 docker run -d --name csss_website_test csss_website_test
 
 
