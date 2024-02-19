570b041a5d72def4074d0080f311f90d37075691
Jace Manshadi <jaymanshad@proton.me>
2024-02-19 13:54:15 -0800

    updating the email so it's more clear to the Director of Archives what they have to do

--- csss-site/src/about/views/input_new_officers/enter_new_officer_info/utils/alert_doa_to_update_email_list.py
+++ csss-site/src/about/views/input_new_officers/enter_new_officer_info/utils/alert_doa_to_update_email_list.py
@@ -24,12 +24,28 @@ def alert_doa_to_update_email_list(sfu_officer_mailing_list_email, position_has_
     if settings.DEBUG:
         url += f":{settings.PORT}"
     url += f'/about/{CURRENT_EMAIL_MAPPINGS_URL}'
-    message = f"get latest mappings at {url}"
+    message = """Hi There!
+Seems there has been a change in who holds a CSSS elected or appointed position.
+
+YOU NEED TO TAKE THE FOLLOWING ACTIONS:
+"""
+    message += (
+        f"1. Go to {url} to see who is supposed to be in the maillist {sfu_officer_mailing_list_email}"
+        f" [and if it exists ,{sfu_officer_mailing_list_email}-current]\n\n"
+        f"2. Go to https://maillist.sfu.ca/ , log in with your SFUID and update the {sfu_officer_mailing_list_email}"
+        f" [and if it exists ,{sfu_officer_mailing_list_email}-current]"
+        f" on there to match what you see on {url} for {sfu_officer_mailing_list_email} "
+        f"[and if it exists, {sfu_officer_mailing_list_email}-current]\n\n"
+    )
     if position_has_bitwarden_access:
-        message += (" and let the Sys Admin know to perform a takeover "
-                    "at https://vault.bitwarden.com/#/settings/emergency-access")
+        message += (
+            '3. Then, message the Systems Administrator to let them know they have to "perform a bitwarden takeover'
+            f' of account {sfu_officer_mailing_list_email} at '
+            'https://vault.bitwarden.com/#/settings/emergency-access'
+        )
     success, error_message = gmail.send_email(
-        f"update {sfu_officer_mailing_list_email}", message, "csss-doa-current@sfu.ca", "Director of Archives",
+        f"ACTION NEEDED FOR UPDATING CSSS MAILLIST {sfu_officer_mailing_list_email}", message,
+        "csss-doa-current@sfu.ca", "Director of Archives",
         from_name="SFU CSSS Website"
     )
     if not success:
