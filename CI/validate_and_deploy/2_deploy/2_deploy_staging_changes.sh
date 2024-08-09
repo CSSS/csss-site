e851a223e6f51e59433cfc26eae6a13bc59fa683
Jace Manshadi <jaymanshad@proton.me>
2024-08-09 16:03:58 -0700

    need to specifically set the setuptools version as it's automatically set to version 7.0.0 now which causes an issue with django_allauth

--- CI/validate_and_deploy/2_deploy/2_deploy_staging_changes.sh
+++ CI/validate_and_deploy/2_deploy/2_deploy_staging_changes.sh
@@ -24,6 +24,7 @@ function setup_master_virtual_env(){
   rm -fr "${BASE_DIR}/envCSSS_master"
   python3 -m virtualenv envCSSS_master
   . "${BASE_DIR}/envCSSS_master/bin/activate"
+  python3 -m pip install setuptools==68.2.2 --no-cache-dir
   python3 -m pip install -r "${BASE_DIR}/csss-site/requirements.txt" --no-cache-dir
 
 
@@ -48,6 +49,7 @@ function switch_to_pr_branch(){
   cd "${BASE_DIR}"
   python3 -m virtualenv envCSSS
   . "${BASE_DIR}/envCSSS/bin/activate"
+  python3 -m pip install setuptools==68.2.2 --no-cache-dir
   python3 -m pip install -r "${BASE_DIR}/csss-site/requirements.txt" --no-cache-dir
 }
 
